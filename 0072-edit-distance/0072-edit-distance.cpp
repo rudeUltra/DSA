a2ab2aea1497d8db39d9ce30c20b117b9d93548b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-03-30 15:33:56 +0530

    Time: 91 ms (9.50%), Space: 81 MB (8.67%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        //striver god :))
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return stringmatch(n-1,m-1,word1,word2,dp);
+    }
+    int stringmatch(int i,int j,string word1,string word2,vector<vector<int>>&dp){
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+        //explore all possibilities
+        if(dp[i][j]!=-1){
+            return dp[i][j]; //overlapping subproblems
+        }
+        //if match noice no need to edit
+        if(word1[i]==word2[j]){
+            return dp[i][j]=stringmatch(i-1,j-1,word1,word2,dp)+0;
+        }
+        //explore all possibilites dr strange xD
+        return dp[i][j] = 1+min(stringmatch(i,j-1,word1,word2,dp),min(stringmatch(i-1,j-1,word1,word2,dp),stringmatch(i-1,j,word1,word2,dp)));
+            
+        
+    }
+};
