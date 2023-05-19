b19600ad331da9a3985d7bd8294fff8fa293e4af
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-05-19 14:13:27 +0530

    Time: 25 ms (67.06%), Space: 13.6 MB (41.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    bool dfs(int node,int color,vector<int>&vis,vector<int>adj[]){
-        //vis array will hold the color
-        vis[node]=color;
-        for(auto it:adj[node]){
-            if(vis[it]==-1){
-                if(!dfs(it,!color,vis,adj)){
-                    return false;
+    
+    bool f(int node,vector<vector<int>>&graph,vector<int>&color,int col){
+        color[node]=col; //mark it visited
+        bool x=true;
+        for(auto it:graph[node]){
+            if(color[it]==-1){
+                //not visited go ahead dfs
+                bool x=f(it,graph,color,1-col); //opposite color
+                if(x==false){
+                    return false; //just check the negative condition bix
                 }
             }
-            else{
-                //already visited must be a parent so chekc parents color/value
-                if(vis[it]==vis[node]){
+            else if(color[it]!=-1){
+                if(color[it]==color[node]){
+                    //sus can't partition into 2 sets sed
                     return false;
                 }
             }
         }
+        
         return true;
     }
+    
     bool isBipartite(vector<vector<int>>& graph) {
-        //convert adjacency matrix to adjacency list hm.
+        //no need to convert into a adj list pretty straighforward hm
         int n=graph.size();
-        vector<int>vis(n,-1); //initially all color -1
-        vector<int>adj[n];
+        vector<int>color(n,-1); //modified visited array hm
+        
+        //1-red 0-blue
+        //there are multiple components sed
+        
         for(int i=0;i<n;++i){
-            for(int j=0;j<graph[i].size();++j){
-                adj[i].push_back(graph[i][j]);
-            }
-        }
-        for(int i=0;i<n;++i){
-            //0 based indexing
-            if(vis[i]==-1){
-                if(!dfs(i,1,vis,adj)){
-                    return false; //initial color is -1
+            if(color[i]==-1){
+                if(!f(i,graph,color,1)){
+                    return false;
                 }
             }
         }
-        return true;
         
+        return true;
     }
 };
