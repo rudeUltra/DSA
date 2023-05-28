d9f48e02f31117f95f4095df539a7588717ae2d8
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-05-28 12:09:49 +0530

    Time: 43 ms (97.74%), Space: 8.2 MB (99.77%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,80 +1,26 @@
 class Solution {
 public:
+    int dp[101][101];
     
-    /*time and space complexity
-    n===>size of cuts array
-    time:(n^2)
-    if there are cuts at every unit distance then the depth of recursion is at max n
-    and the every time n calls are called so (n*n)
-    
-    space:(n^2) //because we are maintaining a matrix of nxn
-    */
-    
-     //taking size 103 instead of 100 because i have added two element of my own in the cuts array
-    int dp[103][103];
-    
-    Solution()
+    int mincost(int start, int end, vector<int>& cuts, int cuts_start, int cuts_end)
     {
-        //initializing dp table with -1
-        memset(dp,-1,sizeof(dp));
-    }
-    
-    
-    //do not forget to pass vect as reference(&) if you remove this
-    //it will result into tle
-    int solve(int start,int end,vector<int> &vect)
-    {
-        // printf("%d %d\n",start,end);
-        //base case
-        if(end-start == 1)
+        if(cuts_start>cuts_end)
             return 0;
-        //if already know the ans return it
-        if(dp[start][end]!=-1)
-            return dp[start][end];
         
-        //calculating length of the stick
-        //it is the stick between two cuts: the starting and the ending cut
-        int len = vect[end]-vect[start];
-        int ans = INT_MAX;
+        if(dp[cuts_start][cuts_end]!=-1)
+            return dp[cuts_start][cuts_end];
         
-        //traversing all the cuts
-        //it starts from just next to start
-        //because we cannot cut the cuts smaller than  the starting index
-        for(int i=start+1;i<end;i++)
-        {
-            // recursively solving for  
-            //left part   +   right part
-            int temp = solve(start,i,vect) + solve(i,end,vect);
-            
-            //maintaining the minimum after every call
-            ans = min(ans,temp);
-        }
+        int minimum = INT_MAX;
+        for(int i=cuts_start; i<=cuts_end; i++)
+            minimum = min(minimum, (end-start)+mincost(start, cuts[i], cuts, cuts_start, i-1)+mincost(cuts[i], end, cuts, i+1, cuts_end));
         
-       return dp[start][end] = len+ans;
+        return dp[cuts_start][cuts_end] = minimum;
     }
-    
     
     int minCost(int n, vector<int>& cuts) {
-        //sort the cuts
+
+        memset(dp,-1,sizeof(dp));
         sort(cuts.begin(),cuts.end());
-        
-        //creating new vector
-        vector<int> vect;
-        
-        //pushing 0
-        vect.push_back(0);
-        //pushing the cuts vector
-        for(int a:cuts)
-        {
-            vect.push_back(a);
-        }
-        //pushing n
-        vect.push_back(n);
-        
-        
-        return solve(0,vect.size()-1,vect);
-        
-        
-        
+        return mincost(0, n, cuts, 0, cuts.size()-1);
     }
 };
