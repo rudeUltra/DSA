849b950b9cef579cd5a7c30140cb01df6e074b7e
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-06-13 17:43:33 +0530

    Time: 50 ms (8.17%), Space: 17.5 MB (14.98%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        
+        int n=g.size();
+        int m=s.size();
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<n && j<m){
+            //two pointer less goo
+            if(g[i]<=s[j]){
+                ++i;
+                ++j; //cookie assigned
+                count++;
+            }
+            else{
+                ++j;
+            }
+        }
+        return count;
+    }
+};
