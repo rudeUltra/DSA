daba52317d9a2883c5aacad0d7a1d6f4bf2c085b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-06-25 17:21:28 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    
+    int f(int n,vector<int>&dp){
+        if(n<=2){
+            return n; //can only take 1 step
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }
+    
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
+};
