5580193033683a4c53fdbd6a0ed783b6bbed8b8b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-06-26 18:48:04 +0530

    Time: 354 ms (58.81%), Space: 94.9 MB (42.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-    bool subset(int index,int target,vector<int>&arr,vector<vector<int>>&dp){
-    if(target==0){
-        return true;
-    }
-    if(index==0){
-        //only one option take
-        if(target==arr[0]){
-            return true;
+    
+    bool f(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
+        if(target==0){
+            return 1; //siu
         }
-        else{
-            return false;
+        if(idx==0){
+            return target==nums[idx]; //true if equal siu
         }
+        
+        if(dp[idx][target]!=-1){
+            return dp[idx][target];
+        }
+        int pick=0;
+        int notpick=f(idx-1,target,nums,dp);
+        if(nums[idx]<=target){
+            pick=f(idx-1,target-nums[idx],nums,dp);
+        }
+        return dp[idx][target]=pick | notpick;
     }
-    if(dp[index][target]!=-1){
-        return dp[index][target];
-    }
-    bool notake=subset(index-1,target,arr,dp);
-    bool take=false;
-    if(arr[index]<=target){
-        take=subset(index-1,target-arr[index],arr,dp);
-    }
-    return dp[index][target]=take||notake; //or
-     
-}
+    
+    
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-      
-       for(int i=0;i<n;++i){
-           sum=sum+nums[i];
-       }
+        for(int i=0;i<n;++i){
+            sum+=nums[i];
+        }
         if(sum%2==1){
             return false;
         }
-        else{
-            //sum=20 if 10 is found other is 10 only xD logic
-             vector<vector<int>>dp(n,vector<int>((sum/2)+1,-1));
-             if(subset(n-1,sum/2,nums,dp)){
-                 return true;
-             }
-            else{
-                return false;
-            }
-        }
+        int target=sum/2;
+        
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        
+        return f(n-1,target,nums,dp);
     }
 };
