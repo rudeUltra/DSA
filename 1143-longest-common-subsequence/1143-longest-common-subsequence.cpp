e95874c80cc0a989c8029c216e05f1f0b914be25
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-06-28 02:27:21 +0530

    Time: 67 ms (59.92%), Space: 24.5 MB (24.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
-    int f(int i,int j,string &s,string &t,vector<vector<int>>&dp){
-        if(i<0 || j<0){
-            return 0; //no more lcs can be formed siu
+    
+    int f(int i,int j,string text1,string text2,vector<vector<int>>&dp){
+        if(i>=text1.size() || j>=text2.size()){
+            return 0; //base no more can be found
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(s[i]==t[j]){
-            return dp[i][j]=1+f(i-1,j-1,s,t,dp);
+        
+        //two options either match or no match if match then ez pick the character obvs
+        if(text1[i]==text2[j]){
+            return dp[i][j]=1+f(i+1,j+1,text1,text2,dp);
         }
-        return dp[i][j]=max(f(i,j-1,s,t,dp),f(i-1,j,s,t,dp)); //recursion
+        
+        return dp[i][j]=max(f(i+1,j,text1,text2,dp),f(i,j+1,text1,text2,dp));
     }
+    
+    
     int longestCommonSubsequence(string text1, string text2) {
+        //2 pal ka aram de
+        //lets try tabulations first
         int n=text1.size();
         int m=text2.size();
+        
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       //ok recursion not gonna work sad xD
         
         for(int i=1;i<=n;++i){
             for(int j=1;j<=m;++j){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]); //try all possibilities
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         
         return dp[n][m];
-        
     }
 };
