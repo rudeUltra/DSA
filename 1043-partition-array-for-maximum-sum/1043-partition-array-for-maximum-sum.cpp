78d359b5d042fc224530b6f39d91e4a83f45edfd
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-06-30 19:51:39 +0530

    Time: 23 ms (42.28%), Space: 8.5 MB (48.04%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
+    
     int f(int idx,int k,vector<int>&arr,vector<int>&dp){
-        if(idx==arr.size()){
-            return 0;
+        if(idx>=arr.size()){
+            return 0; //siu
         }
+        int ans=INT_MIN;
+        int mx=INT_MIN;
+        int n=arr.size();
+        int len=0;
         
         if(dp[idx]!=-1){
             return dp[idx];
         }
-        int mx=INT_MIN;
-        int ans=INT_MIN;
-        int length=0;
-        int n=arr.size();
-        for(int j=idx;j<min(idx+k,n);++j){
-            length++;
-            mx=max(mx,arr[j]);
-            int sum=f(j+1,k,arr,dp)+(length*mx);
-            ans=max(sum,ans);
+        //at every index partition siu
+        for(int i=idx;i<min(idx+k,n);++i){
+            //cool trick ngl xD
+            mx=max(mx,arr[i]);
+            len++; //diff for each subarray xD cant do i+1.. 
+            int temp=f(i+1,k,arr,dp)+(mx*len);
+            ans=max(ans,temp);
         }
         
         return dp[idx]=ans;
-        
     }
     
     
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
         vector<int>dp(n,-1);
+        
         return f(0,k,arr,dp);
     }
 };
