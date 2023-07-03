4e5e822242cc0c9b77c739a60d5660188e13061a
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-03 21:48:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (78.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
+        int n=s.size();
         stack<char>st;
-        for(int i=0;i<s.size();++i){
+        for(int i=0;i<n;++i){
             if(s[i]=='(' || s[i]=='{' || s[i]=='['){
                 st.push(s[i]);
             }
             else{
-               if(st.empty() || (st.top()=='(' && s[i]!=')') || (st.top()=='{' && s[i]!='}') || (st.top()=='[' && s[i]!=']')){
-                   return false;
-               }
-                else{
-                    st.pop();
-                    
+                if(st.size()==0){
+                    return false;
                 }
+                if((s[i]==')' && st.top()!='(') || (s[i]=='}' && st.top()!='{')  || (s[i]==']' && st.top()!='[') ){
+                    return false;
+                }
+                st.pop();
             }
         }
-        if(st.empty()){
+        if(st.size()==0){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
 };
