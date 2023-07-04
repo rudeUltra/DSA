6d78d448654b1fb44d63cd490244df7503b90ecf
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-04 13:36:02 +0530

    Time: 11 ms (11.27%), Space: 10 MB (95.35%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,26 +4,29 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        if(n==1){
-            nums[0]; //basically pivot finding siu
-        }
-        if(nums[low]<nums[high]){
-            //rotated n times already sorted so
-            return nums[0];
-        }
+        
         while(low<=high){
-            int mid=low+(high-low)/2; //integer overflow
-            if(mid>0 && nums[mid-1]>nums[mid]){
-                //found the pivot siu
+            int mid=low+(high-low)/2;
+            cout<<nums[mid]<<" ";
+            if(mid>0 && nums[mid]<nums[mid-1]){
+                //pivot found siu
                 return nums[mid];
             }
-            if(nums[mid]>=nums[low] && nums[mid]>=nums[high]){
-                low=mid+1; //search in the unsorted array siu pls make sure right part is also unsorted otherwise edge cases siu
+            
+            if(nums[mid]>nums[low] && nums[mid]<nums[high]){
+                //sorted array bc!
+                return nums[low];
+            }
+            
+            
+            else if(nums[high]<nums[mid]){
+                low=mid+1;
             }
             else{
-                high=mid-1; 
+                high=mid-1;
             }
         }
-        return nums[0]; //will never hit but k
+        cout<<low<<" "<<high;
+        return nums[low];
     }
 };
