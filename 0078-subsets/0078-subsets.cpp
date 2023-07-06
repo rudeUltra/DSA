4eaf14cb6ffafc5f7154e2c03d96a911e0db5ac2
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-06 16:47:09 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (60.79%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    
+    void dfs(int idx,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+        if(idx>=nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        //pick and not pick
+        temp.push_back(nums[idx]);
+        dfs(idx+1,nums,temp,ans);
+        temp.pop_back();
+        dfs(idx+1,nums,temp,ans);
+    }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
+        //lets try 2 ways 1st pick not pick then bit monipulation xD
+        vector<int>temp;
         vector<vector<int>>ans;
         int n=nums.size();
-        for(int num=0;num<(1<<n);++num){
-            //0 1 2 3 4 5 etc
-            vector<int>temp;
-            for(int i=0;i<n;++i){
-                if(num&(1<<i)){
-                    //i'th bit is set siu
-                    temp.push_back(nums[i]);
-                }
-            }
-            ans.push_back(temp);
-        }
+        
+        dfs(0,nums,temp,ans);
         
         return ans;
     }
