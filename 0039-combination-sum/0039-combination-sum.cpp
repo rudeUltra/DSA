2ab8ed1c194d5389a1b1a3f321a21ba4c97b7ac7
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-18 18:52:21 +0530

    Time: 4 ms (84.49%), Space: 10.9 MB (54.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    
-    
-    
-    
+    vector<vector<int>>ans;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // sort(candidates.begin(),candidates.end()); //sort hmm important
-        vector<vector<int>> ans;
-        vector<int>current;
-        backtrack(ans,current,candidates,target,0);
+        //basically pick not pick xD`
+        vector<int>curr;
+        f(0,candidates,target,curr);
+        
         return ans;
     }
-    void backtrack(vector<vector<int>>&ans,vector<int>&current,vector<int>&candidates,int target,int idx){
-        if(target==0){
-            ans.push_back(current);
+    
+    void f(int index,vector<int>&candidates,int target,vector<int>&curr){
+        
+        if(index>=candidates.size()){
             return;
         }
         
-        for(int i=idx;i<candidates.size() ;++i){
-            if(candidates[i]<=target){
-            current.push_back(candidates[i]);
-            backtrack(ans,current,candidates,target-candidates[i],i);
-            current.pop_back();
-            }
+        if(target==0){
+            ans.push_back(curr);
+            return;
         }
+        //pick not pick
+        
+        if(candidates[index]<=target){
+            curr.push_back(candidates[index]);
+            f(index,candidates,target-candidates[index],curr);
+            curr.pop_back();
+        }
+        f(index+1,candidates,target,curr);
     }
-    
-    
 };
