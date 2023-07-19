8fdb25241d97a975254e4158c8d4881ace13759d
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-19 12:40:22 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (13.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,32 +3,35 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i=0;
         int j=0;
-        int k=0;
-        vector<int> a;
+        vector<int>ans;
+        
+       
+        
         while(i<m && j<n){
             if(nums1[i]<nums2[j]){
-                a.push_back(nums1[i]);
-                ++i;
-                ++k;
+                ans.push_back(nums1[i]);
+                i++;
             }
             else{
-                a.push_back(nums2[j]);
-                ++k;
+                ans.push_back(nums2[j]);
                 ++j;
             }
         }
+        
         while(i<m){
-            a.push_back(nums1[i]);
-            ++i;
-            ++k;
+             ans.push_back(nums1[i]);
+                i++;
         }
         while(j<n){
-            a.push_back(nums2[j]);
-            ++j;
-            ++k;
+             ans.push_back(nums2[j]);
+                j++;
         }
-        for(int i=0;i<k;++i){
-            nums1[i]=a[i];
+        
+        nums1.resize(n+m);
+        
+        for(int i=0;i<ans.size();++i){
+            nums1[i]=ans[i];
         }
+        
     }
 };
