1483788f4d6a0afd9c0e83a71483c300d94bbf97
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-19 18:16:33 +0530

    Time: 11 ms (46.85%), Space: 17.5 MB (6.97%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    
-    int f(int idx,int buy,vector<int>&prices,vector<vector<int>>&dp){
+    int f(int idx,vector<int>&prices,int buy,vector<vector<int>>&dp){
         if(idx>=prices.size()){
-            return 0; //no more profit sed
+            return 0;
         }
+        
         if(dp[idx][buy]!=-1){
             return dp[idx][buy];
         }
         int ans=0;
         if(buy){
-            int notpick=f(idx+1,buy,prices,dp);
-            int pick=f(idx+1,0,prices,dp)-prices[idx];
+            int notpick=f(idx+1,prices,buy,dp);
+            int pick=f(idx+1,prices,0,dp)-prices[idx];
+            
             ans=max(pick,notpick);
         }
         else{
-            int notsell=f(idx+1,buy,prices,dp);
-            int sell=f(idx+1,1,prices,dp)+prices[idx];
+            int sell=f(idx+1,prices,1,dp)+prices[idx];
+            int notsell=f(idx+1,prices,buy,dp);
             ans=max(sell,notsell);
         }
-        return dp[idx][buy]=ans; //for each index explore all possibilites buy sell or not xD
+        
+        return dp[idx][buy]=ans;
     }
     
     
+    
     int maxProfit(vector<int>& prices) {
-        //rmemeber in stock cant do top down n-1 m-1 since we didnt buy stock kaise predict the future sed
-        
-        //two options buy or sell if i can buy or sell should i tAke the decision or not xD
-        
-        //if else conditionts remember to explore all possibilites siu
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1)); //1 for buy 0 for sell xD
+        vector<vector<int>>dp(n,vector<int>(2,-1));
         
-        return f(0,1,prices,dp);
+        return f(0,prices,1,dp);
+        
     }
 };
