5fc085cdb180d07fc4270b2bafcbfdb0f302a1ec
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-21 12:25:25 +0530

    Time: 145 ms (90.59%), Space: 13.3 MB (25.03%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    
     int findNumberOfLIS(vector<int>& nums) {
-        const int n = nums.size();
-        vector<int> lis(n,1);  // stores length of longest sequence till i-th position
-        vector<int> count(n,1);  // stores count of longest sequence of length lis[i]
-        int maxLen = 1;  // maximum length of lis
-		
-		// O(N^2) DP Solution
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
+        int n=nums.size();
+        vector<int>dp(n,1);
+        
+        if(n==1){
+            return 1;
+        }
+        vector<int>ways(n,1);
+        int mx=INT_MIN;
+        for(int i=1;i<n;++i){
+            for(int j=0;j<i;++j){
                 if(nums[i]>nums[j]){
-                    if(lis[j] + 1 > lis[i]){ // strictly increasing
-                        lis[i] = lis[j] + 1;
-                        count[i] = count[j];
-                    } 
-					 // this means there are more subsequences of same length ending at length lis[i]
-					else if(lis[j]+1 == lis[i]){ 
-                        count[i] += count[j];
+                    if(dp[i]<dp[j]+1){
+                        dp[i]=dp[j]+1;
+                        ways[i]=ways[j];
+                    }
+                    else if(dp[i]==dp[j]+1){
+                        //same lenght lIS hm
+                        ways[i]+=ways[j];
                     }
                 }
             }
-            maxLen = max(maxLen,lis[i]);
+            mx=max(mx,dp[i]);
         }
         
-        int numOfLIS = 0;
-        // count all the subseq of length maxLen
-        for(int i=0;i<n;i++){
-            if(lis[i]==maxLen)
-                numOfLIS += count[i];
+        int ans=0;
+        
+        for(int i=0;i<n;++i){
+            if(dp[i]==mx){
+                ans+=ways[i];
+            }
         }
-            
-        return numOfLIS;
+        
+        return ans;
     }
 };
