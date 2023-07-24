e9bf655502332102bd3ba735601e5ced05bfca99
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-07-24 11:47:41 +0530

    Time: 214 ms (9.40%), Space: 57.9 MB (5.20%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
+    int m;
+    int n;
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>> dis(n,vector<int>(m,-1));
-        queue<pair<int,int>> Q;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(mat[i][j]==0){
-                    dis[i][j]=0;
-                    Q.push({i,j});
+        m = mat.size();
+        n = mat[0].size();
+        vector<vector<int>> matrix(m, vector<int>(n, 0));
+        vector<vector<bool>> seen(m, vector<bool>(n, false));
+        queue<vector<int>> queue;
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                matrix[row][col] = mat[row][col];
+                if (matrix[row][col] == 0) {
+                    queue.push({row, col, 0});
+                    seen[row][col] = true;
                 }
             }
         }
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        while(!Q.empty()){
-            pair<int,int> current=Q.front();
-            Q.pop();
-            for(int i=0;i<4;++i){
-                pair<int,int> neighbor={current.first+dx[i],current.second + dy[i]};
-                if(neighbor.first>=0 && neighbor.first<n && neighbor.second>=0 && neighbor.second<m && dis[neighbor.first][neighbor.second]==-1){
-                    dis[neighbor.first][neighbor.second]=dis[current.first][current.second]+1;
-                    Q.push({neighbor.first,neighbor.second});
-                }
-            }
-        }
-        return dis;
         
+        while (!queue.empty()) {
+            vector<int> curr = queue.front();
+            queue.pop();
+            int row = curr[0], col = curr[1], steps = curr[2];
+            
+            for (vector<int>& direction: directions) {
+                int nextRow = row + direction[0], nextCol = col + direction[1];
+                if (valid(nextRow, nextCol) && !seen[nextRow][nextCol]) {
+                    seen[nextRow][nextCol] = true;
+                    queue.push({nextRow, nextCol, steps + 1});
+                    matrix[nextRow][nextCol] = steps + 1;
+                }
+            }
+        }
+        
+        return matrix;
+    }
+    
+    bool valid(int row, int col) {
+        return 0 <= row && row < m && 0 <= col && col < n;
     }
 };
