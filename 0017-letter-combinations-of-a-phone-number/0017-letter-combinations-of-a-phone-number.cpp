78ad47f91a0cd303f52c9d37acfa70c775e9d461
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-03 12:28:49 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (5.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    //try all possibilities siu
-   unordered_map<char,string>mp;
-    void f(string digits,string &temp,vector<string>&ans,int index){
-        if(index==digits.size()){
+    
+    void dfs(int idx,string digits,string &temp,vector<string>&ans,unordered_map<char,string>mp){
+        if(idx>=digits.size()){
             ans.push_back(temp);
             return;
         }
-        string curr=mp[digits[index]];
-        for(int i=0;i<mp[digits[index]].size();++i){
-            temp.push_back(curr[i]);
-            f(digits,temp,ans,index+1);
+        
+        for(auto it:mp[digits[idx]]){
+            temp.push_back(it);
+            dfs(idx+1,digits,temp,ans,mp);
             temp.pop_back();
         }
-        
     }
+    
+    
     vector<string> letterCombinations(string digits) {
-        //backtracking solution interesting
-         
-    mp['2']="abc";
-    mp['3']="def";
-    mp['4']="ghi";
-    mp['5']="jkl";
-    mp['6']="mno";
-    mp['7']="pqrs";
-    mp['8']="tuv";
-    mp['9']="wxyz";
-
+        unordered_map<char,string>mp;
+        int n=digits.size();
+        
+       
+        
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
+        
+        string temp;
         vector<string>ans;
-        if(digits.size()==0){
+         if(n==0){
             return ans;
         }
-        string temp=""; //temp string
-        f(digits,temp,ans,0);
-
+        
+        dfs(0,digits,temp,ans,mp);
+        
         return ans;
     }
 };
