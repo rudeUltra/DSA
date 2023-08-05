8351e38116f77ddc031897ff8e226951e7725bbb
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-05 15:43:37 +0530

    Time: 12 ms (91.60%), Space: 16.3 MB (16.75%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -11,35 +11,38 @@
  */
 class Solution {
 public:
-   vector<TreeNode*> buildTree(int start, int end) {
-	vector<TreeNode*> ans;
-        
-    // If start > end, then subtree will be empty so add NULL in the ans and return it.
-    if(start > end) {
-		ans.push_back(NULL);
-        return ans;
-    }
-
-    // Iterate through all values from start to end to construct left and right subtree recursively
-	for(int i = start; i <= end; ++i) {
-		vector<TreeNode*> leftSubTree = buildTree(start, i - 1);    // Construct left subtree
-        vector<TreeNode*> rightSubTree = buildTree(i + 1, end);     // Construct right subtree
-            
-		// loop through all left and right subtrees and connect them to ith root  
-		for(int j = 0; j < leftSubTree.size(); j++) {
-			for(int k = 0; k < rightSubTree.size(); k++) {
-				TreeNode* root = new TreeNode(i);   // Create root with value i
-				root->left = leftSubTree[j];   // Connect left subtree rooted at leftSubTree[j]
-                root->right = rightSubTree[k];   // Connect right subtree rooted at rightSubTree[k]
-				ans.push_back(root);    // Add this tree(rooted at i) to ans data-structure
-			}
-		}
-    }
-        
-	return ans;
-}
     
-vector<TreeNode*> generateTrees(int n) {
-	return buildTree(1, n);
-}
+    vector<TreeNode*>dfs(int i,int j){
+        
+        vector<TreeNode*>ans;
+        if(i>j){
+            ans.push_back(NULL);
+            return ans;
+        }
+        for(int k=i;k<=j;++k){
+            //split siu
+            vector<TreeNode*>left1=dfs(i,k-1);
+            vector<TreeNode*>right1=dfs(k+1,j);
+            
+            //create Root sIU
+            for(auto it:left1){
+                for(auto it1:right1){
+                    //all combinations siu
+                    TreeNode*root=new TreeNode(k);
+                    root->left=it;
+                    root->right=it1;
+                    
+                    ans.push_back(root);
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {
+        
+        return dfs(1,n);
+    }
 };
