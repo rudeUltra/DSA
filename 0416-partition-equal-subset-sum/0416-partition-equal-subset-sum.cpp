a260e67fc62aad9eb360cb5e71f61c6d2355dfbe
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-15 22:13:47 +0530

    Time: 264 ms (67.95%), Space: 178 MB (11.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     
-    bool f(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
+    
+    bool f(int idx,vector<int>&nums,int target,vector<vector<int>>&dp){
         if(target==0){
-            return 1; //siu
+            return true;
         }
-        if(idx==0){
-            return target==nums[idx]; //true if equal siu
+        if(idx>=nums.size()){
+            return false;
         }
-        
         if(dp[idx][target]!=-1){
             return dp[idx][target];
         }
-        int pick=0;
-        int notpick=f(idx-1,target,nums,dp);
+        
+        bool notpick=f(idx+1,nums,target,dp);
+        bool pick=false;
+        
         if(nums[idx]<=target){
-            pick=f(idx-1,target-nums[idx],nums,dp);
+            pick=f(idx+1,nums,target-nums[idx],dp);
         }
+        
         return dp[idx][target]=pick | notpick;
     }
     
-    
     bool canPartition(vector<int>& nums) {
+        //siu
         int n=nums.size();
         int sum=0;
         for(int i=0;i<n;++i){
             sum+=nums[i];
         }
-        if(sum%2==1){
-            return false;
+        
+        if(sum%2!=0){
+            //gotemm
+            return false; //cant divide into 2 subsets hm
         }
-        int target=sum/2;
-        
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        
-        return f(n-1,target,nums,dp);
+        vector<vector<int>>dp(n,vector<int>(sum,-1));
+        return f(0,nums,sum/2,dp);
     }
 };
