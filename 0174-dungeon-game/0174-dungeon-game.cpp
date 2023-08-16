ff6a38927d89e39c8b8cbf9a9a7b94a09d2ea696
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-16 22:41:49 +0530

    Time: 4 ms (89.27%), Space: 8.8 MB (71.27%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    int solve(int i, int j, int m , int n, vector<vector<int>> &grid, vector<vector<int>> &dp)
+    int dp[200][200];
+    int solve(vector<vector<int>> &grid, int n, int m, int i, int j) 
     {
-        // if we come out of the grid simply return a large value
-        if(i >= m || j >= n)
+        if(i >= n || j >= m) 
             return INT_MAX;
-        
-        if(dp[i][j] != INT_MAX)
+        if(i == n-1 && j == m-1) 
+            return grid[i][j] <= 0 ? abs(grid[i][j]) + 1 : 1;
+        if (dp[i][j] != -1) 
             return dp[i][j];
-        
-        // calucate health by the 2 possible ways
-        int down = solve(i + 1, j, m, n, grid, dp);
-        int right = solve(i, j + 1, m, n, grid, dp);
-        
-        int health = min(down, right);
-        
-        // we reach the destination when both the sides return INT_MAX
-        if(health == INT_MAX)
-        {
-            health = 1; // both are +ve large integers so min health required = 1
-        }
-        
-        int ans = 0;
-        if(health - grid[i][j] > 0)
-        {
-            ans = health - grid[i][j];
-        }
-        else
-        {
-            ans = 1;
-        }
-             
-        return dp[i][j] = ans;
+        int right = solve(grid, n, m, i, j+1);
+        int down = solve(grid, n, m, i+1, j);
+        int ans = min(right, down) - grid[i][j];
+        return dp[i][j] = ans <=0 ? 1 : ans;
     }
-    
-    int calculateMinimumHP(vector<vector<int>>& dungeon) 
+    int calculateMinimumHP(vector<vector<int>> &dungeon) 
     {
-        int m = dungeon.size();
-        int n = dungeon[0].size();
-        
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, INT_MAX));
-        
-        return solve(0, 0, m, n, dungeon, dp);
+        if(dungeon.size() == 0) 
+            return 0;
+        memset(dp, -1, sizeof(dp));
+        return solve(dungeon, dungeon.size(), dungeon[0].size(), 0, 0);
     }
 };
