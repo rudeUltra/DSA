e5c7fe68321c48d2234cc4e7ee417b9dfcc79c01
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-28 21:29:00 +0530

    Time: 4 ms (79.26%), Space: 10.7 MB (66.98%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        //basically pick not pick xD`
-        vector<int>curr;
-        f(0,candidates,target,curr);
+    
+    
+    void f(int idx,vector<int>&candidates,int target,vector<int>&temp,vector<vector<int>>&ans){
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(idx==candidates.size()){
+            return;
+        }
+        //pick
+        if(candidates[idx]<=target){
+            temp.push_back(candidates[idx]);
+            f(idx,candidates,target-candidates[idx],temp,ans);
+            temp.pop_back();
+        }
         
-        return ans;
+        f(idx+1,candidates,target,temp,ans);
     }
     
-    void f(int index,vector<int>&candidates,int target,vector<int>&curr){
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        vector<vector<int>>ans;
         
-        if(index>=candidates.size()){
-            return;
-        }
+        //Glorified pick or not pick xD
+        f(0,candidates,target,temp,ans);
         
-        if(target==0){
-            ans.push_back(curr);
-            return;
-        }
-        //pick not pick
-        
-       
-        f(index+1,candidates,target,curr);
-         if(candidates[index]<=target){
-            curr.push_back(candidates[index]);
-            f(index,candidates,target-candidates[index],curr);
-            curr.pop_back();
-        }
+        return ans;
     }
 };
