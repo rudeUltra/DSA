b84b1413b1708dbc50d5e7f576a43b493c8ef8c8
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-29 23:01:13 +0530

    Time: 3 ms (53.13%), Space: 8.2 MB (11.17%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    
-    
-    void dfs(vector<int>&nums,vector<int>&temp,unordered_map<int,int>&mp,vector<vector<int>>&ans){
+    void f(vector<int>&nums,vector<int>&used,vector<int>temp,vector<vector<int>>&ans){
         if(temp.size()==nums.size()){
+            //all the characters are in siu
             ans.push_back(temp);
             return;
         }
+        
+        //Start from beginnning thats the tricky part hm
         for(int i=0;i<nums.size();++i){
-            if(mp[i]==0){
-                //not visited
-                mp[i]=1;
+            if(used[i]==0){
+                used[i]=1;
                 temp.push_back(nums[i]);
-                dfs(nums,temp,mp,ans);
+                f(nums,used,temp,ans);
                 temp.pop_back();
-                mp[i]=0;
+                used[i]=0;
             }
         }
     }
     
     
     vector<vector<int>> permute(vector<int>& nums) {
+        //OH very Noice question hm
+        int n=nums.size();
+        vector<int>used(n,0);
         vector<int>temp;
         vector<vector<int>>ans;
-        unordered_map<int,int>mp;
         
-        dfs(nums,temp,mp,ans);
-        
+        f(nums,used,temp,ans);
         return ans;
     }
 };
