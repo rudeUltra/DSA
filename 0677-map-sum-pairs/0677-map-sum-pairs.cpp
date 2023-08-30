746dda736f200b66d9da8ba08cc69426de88e220
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-30 22:56:14 +0530

    Time: 2 ms (63.52%), Space: 8.2 MB (47.97%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.cpp
+++ 0677-map-sum-pairs/0677-map-sum-pairs.cpp
@@ -1,45 +1,134 @@
-class TrieNode {
-public:
-    int score = 0;
-    TrieNode* child[26];
-};
+unordered_map<string,int>mp;
+
 class MapSum {
 public:
-    int res = 0;
-    /** Initialize your data structure here. */
-    TrieNode* root;
+     
+    
+class Node{
+    public:
+    int flag=0;
+    Node *links[26];
+    int count=0;
+    Node(){
+        for(int i=0;i<26;++i){
+            links[i]=NULL;
+        }
+    }
+    int isPresent(char x){
+        if(links[x-'a']==NULL){
+            return 0;
+        }
+        return 1; //link is there
+    }
+    
+    Node* getLink(char x){
+        return links[x-'a'];
+    }
+    void setLink(char x,Node *temp){
+        links[x-'a']=temp;
+    }
+    
+};
+
+class Trie {
+public:
+    
+    Node*root;
+    Trie() {
+        root=new Node();
+    }
+    
+    void insert1(string word,int x) {
+        int n=word.size();
+        Node*curr=root;
+        for(int i=0;i<n;++i){
+            if(curr->isPresent(word[i])==0){
+                Node *temp=new Node();
+                curr->setLink(word[i],temp);
+            }
+            curr=curr->getLink(word[i]);
+            curr->count+=x;
+        }
+        curr->flag=1; //Indicating that a word is ending here siu
+    }
+    
+     void insert2(string word,int x,int prev) {
+        int n=word.size();
+        Node*curr=root;
+        for(int i=0;i<n;++i){
+            if(curr->isPresent(word[i])==0){
+                Node *temp=new Node();
+                curr->setLink(word[i],temp);
+            }
+            curr=curr->getLink(word[i]);
+            curr->count=(curr->count-prev)+x;
+        }
+        curr->flag=1; //Indicating that a word is ending here siu
+    }
+    
+    
+    int search(string word) {
+        
+        int n=word.size();
+        Node*curr=root;
+        int sum=0;
+        for(int i=0;i<n;++i){
+            if(curr->isPresent(word[i])==0){
+               return sum;
+            }
+            curr=curr->getLink(word[i]);
+            
+        }
+        
+        return sum=curr->count;
+        
+    }
+    
+    bool startsWith(string prefix) {
+        
+        
+         int n=prefix.size();
+        Node*curr=root;
+        for(int i=0;i<n;++i){
+            if(curr->isPresent(prefix[i])==0){
+               return false;
+            }
+            curr=curr->getLink(prefix[i]);
+        }
+        //Ab aa hi gaye hai to xD
+        return true;
+        
+    }
+};
+    
+    Trie*ds;
+   unordered_map<string,int>mp;
+
     MapSum() {
-        root = new TrieNode();
+        ds=new Trie();
     }
     
     void insert(string key, int val) {
-        TrieNode* curr = root;
-        for(char c : key) {
-            if(!curr->child[c-'a']) {
-                curr->child[c-'a'] = new TrieNode();
-            }
-            curr = curr->child[c-'a'];
+        if(mp[key]!=0){
+            //new val hm
+            int prev=mp[key];
+            ds->insert2(key,val,prev);
         }
-        curr->score = val;
+        else{
+            ds->insert1(key,val);
+        }
+        
+        mp[key]=val;
     }
     
     int sum(string prefix) {
-        res = 0;
-        TrieNode* curr = root;
-        for(char c : prefix) {
-            if(!curr->child[c-'a']) 
-                return 0;
-            curr = curr->child[c-'a'];
-        }
-        helper(curr);
-        return res;
-    }
-    void helper(TrieNode* curr) {
-        res += curr->score;
-        for (int i = 0; i < 26; i++) {
-            if (curr->child[i]) {
-                helper(curr->child[i]);
-            }
-        }
+        return ds->search(prefix);
     }
 };
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum* obj = new MapSum();
+ * obj->insert(key,val);
+ * int param_2 = obj->sum(prefix);
+ */
