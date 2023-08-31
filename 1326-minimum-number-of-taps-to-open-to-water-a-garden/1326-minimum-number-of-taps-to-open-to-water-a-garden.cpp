af03772b3ac47fe825538fac04fa11a299c814f8
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-08-31 15:07:53 +0530

    Time: 47 ms (37.74%), Space: 25.8 MB (5.25%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector <vector<int>> intervals;
+        for (int i = 0; i < ranges.size(); i++) {
+            vector<int> inter{i - ranges[i], i + ranges[i]};
+            intervals.push_back(inter);
+        }
+        sort(intervals.begin(), intervals.end(), [](auto &p, auto &q){
+            return (p[0] < q[0]);
+        });
+        int count = 0, start_time = 0, end_time = 0, i = 0;
+        
+        while (end_time < n) {
+            for (; i < intervals.size() && intervals[i][0] <= start_time; i++) {
+                end_time = max(end_time, intervals[i][1]);
+            }
+            if (start_time == end_time) return -1;
+            start_time = end_time;
+            count++;
+        }
+        return count;
+    }
+};
