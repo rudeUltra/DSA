bf7002b435a8bd12dc85734e8201a06302411cec
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-04 00:59:58 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (24.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    
     int f(int i,int j,int n,int m,vector<vector<int>>&dp){
-        if(i<0 || i>=n || j<0 || j>=m){
-            return 0; //sed
-        }
         if(i==0 && j==0){
-            //destination arrived siu
             return 1;
         }
+        if(i<0 || i>=n || j<0 || j>=m){
+            return 0; //no way
+        }
+        
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         
-        return dp[i][j]=f(i-1,j,n,m,dp)+f(i,j-1,n,m,dp);
+        int up=f(i-1,j,n,m,dp);
+        int left=f(i,j-1,n,m,dp);
+        
+        return dp[i][j]=up+left;
     }
     
     
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return f(m-1,n-1,m,n,dp);
         
+        return f(m-1,n-1,m,n,dp);
     }
 };
