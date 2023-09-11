9694e050abb27ad0f13d18f7b15f5745eb6ead60
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-11 15:13:40 +0530

    Time: 3 ms (86.84%), Space: 16.9 MB (32.29%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     
-    
     void f(int idx,vector<int>&candidates,int target,vector<int>&temp,vector<vector<int>>&ans){
         if(target==0){
             ans.push_back(temp);
@@ -10,21 +9,20 @@ public:
         if(idx==candidates.size()){
             return;
         }
-        //pick
+        //Notpick
+        f(idx+1,candidates,target,temp,ans);
+        //Pick
         if(candidates[idx]<=target){
             temp.push_back(candidates[idx]);
             f(idx,candidates,target-candidates[idx],temp,ans);
             temp.pop_back();
         }
         
-        f(idx+1,candidates,target,temp,ans);
     }
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int>temp;
         vector<vector<int>>ans;
         
-        //Glorified pick or not pick xD
         f(0,candidates,target,temp,ans);
         
         return ans;
