018d02a8aafb693a07af709cb65c43a3161f5fef
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-12 00:53:45 +0530

    Time: 4 ms (53.46%), Space: 7.4 MB (94.99%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     
-    void dfs(int idx,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
-        
-        ans.push_back(temp);
-        
+    void f(int idx,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+        ans.push_back(temp); //Push all xD
         int prev=-11;
         for(int i=idx;i<nums.size();++i){
-            cout<<nums[i]<<" ";
             if(nums[i]!=prev){
                 temp.push_back(nums[i]);
-                dfs(i+1,nums,temp,ans);
+                f(i+1,nums,temp,ans);
                 temp.pop_back();
             }
             prev=nums[i];
@@ -19,14 +16,12 @@ public:
     
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // 1 2 2 3 3
-        //basically check in the same for loop did it already occur or not
-        //if it did then for that possibility ending with that number we have already explored OOh
-        //imp thing is in thAt for loop not idx-1 OOOoh
-        sort(nums.begin(),nums.end());
+        //Starting with this index find me all subsets siu
         vector<int>temp;
         vector<vector<int>>ans;
-        dfs(0,nums,temp,ans);
+        sort(nums.begin(),nums.end());
+        
+        f(0,nums,temp,ans);
         
         return ans;
     }
