bc4a90d5d1f9f9a480cedb903d0eb9b2e5c4fa6b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-12 01:39:25 +0530

    Time: 315 ms (80.52%), Space: 8.2 MB (11.59%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,40 +1,86 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        for(int i=0;i<board.size();++i){
-            for(int j=0;j<board[0].size();++j){
-                if(board[i][j]==word[0]){
-                    if(dfs(i,j,board,word,0)){
-                        return true;
-                    }
-                }
-            }
-        }
+    
+    
+//     bool dfs(int i,int j,vector<vector<char>>&board,string &word,int k,vector<vector<int>>&dp){
+        
+        
+//         if(k==word.size()){
+//             //SIUU
+//             return true;
+//         }
+        
+//         if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[k] || dp[i][j]==1){
+//             return false;
+//         }
+       
+//         dp[i][j]=1; //Visited
+        
+//         bool x=dfs(i+1,j,board,word,k+1,dp);
+//         bool y=dfs(i-1,j,board,word,k+1,dp);
+//         bool z=dfs(i,j+1,board,word,k+1,dp);
+//         bool a=dfs(i,j-1,board,word,k+1,dp);
+        
+//         if(x || y || z || a){
+//             return true;
+//         }
+        
+//         dp[j][j]=0;
+        
+//         return false;
+        
+       
+//     }
+    
+    
+bool dfs(int i,int j,vector<vector<char>>&board,string &word,int k,int n,int m,vector<vector<int>>&visited){
+    if(k==word.size()){
+        return true;
+    }
+    if(i<0 || i>=n || j<0 || j>=m || board[i][j]!=word[k] || visited[i][j]==1){
         return false;
     }
 
-     bool dfs(int i,int j,vector<vector<char>>&board,string word,int k){
+    //Check all 4 routes siu
+    //I have to mark myself as Unvisited also OOH big brain
+    //Try out all possibilities if doenst work then GG g bEck
+
+    visited[i][j]=1;
+    bool x=dfs(i-1,j,board,word,k+1,n,m,visited);
+    bool y=dfs(i+1,j,board,word,k+1,n,m,visited);
+    bool z=dfs(i,j-1,board,word,k+1,n,m,visited);
+    bool b=dfs(i,j+1,board,word,k+1,n,m,visited);
+
+    if(x || y || z || b){
+        return true;
+    }
+    visited[i][j]=0;
+    return false;
+
+}
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        //So we have to make sure of the basic backtracking process do the possibilites then backtrack and undo it
+        //Same char can be used in a different way in another dfs call so we have to big brain and unmark it siu
         
-            if(k==word.size()){
-            return true;
+        //DFS + Backtracking
+        int n=board.size();
+        int m=board[0].size();
+        
+       
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(board[i][j]==word[0]){
+                    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+                    if(dfs(i,j,board,word,0,n,m,dp)==1){
+                        return true;
+                    }
+                    
+                }
+            }
         }
         
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[k]){
-            return false;
-        }
-        
-    
-        
-        //4 way dfs
-    
-        //bruh basic dfs rule while exploring make yourself invisible so we dont return to the same place mark yourself visited!
-        char temp=board[i][j];
-        board[i][j]=',';
-        
-        bool x=dfs(i+1,j,board,word,k+1) || dfs(i-1,j,board,word,k+1) || dfs(i,j+1,board,word,k+1) || dfs(i,j-1,board,word,k+1);
-        
-        board[i][j]=temp;
-        
-        return x;
+        //sed 
+        return false;
     }
 };
