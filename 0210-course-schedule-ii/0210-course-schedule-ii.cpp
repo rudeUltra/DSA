28cb0cbe4cc4561429f767c5594573d831a82649
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-18 15:50:03 +0530

    Time: 14 ms (87.09%), Space: 14.4 MB (29.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,59 +1,45 @@
 class Solution {
 public:
-    //we are doing topo sort plus cycle detection in the same function siu
-    
-    bool cycle(int node,vector<int> adj[],vector<int>&path,vector<int>&visited,stack<int>&st){
+        bool isCycle(int node,vector<int>adj[],vector<int>&visited,vector<int>&path,stack<int>&st){
         visited[node]=1;
         path[node]=1;
         
         for(auto it:adj[node]){
-            if(visited[it]==0){
-                if(cycle(it,adj,path,visited,st)){
-                    return true;
-                }
+            if(visited[it]==0 && isCycle(it,adj,visited,path,st)){
+                return true;
             }
-            else if(visited[it]==1 && path[it]==1){
-                //sed
-                return true; //cycle
+            else if(visited[it]==path[it]){
+                //SUS
+                return true;
             }
         }
-        
         path[node]=0;
-        st.push(node);
-        
+        st.push(node); //gotteEMM
         return false;
     }
+    
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        //topo sort + directed graph cycle detection siu
-        
-        if(numCourses==1 && prerequisites.size()==0){
-           return {0}; //SIUU 
-        }
-        
-        vector<int>ans; //to hold the topo sort 
+        //TOPO SORT SIU
         vector<int>visited(numCourses,0);
-        
         vector<int>adj[numCourses];
-        
-        //build the graph
-        
+        vector<int>path(numCourses,0);
+        stack<int>st;
+        vector<int>ans;
         for(int i=0;i<prerequisites.size();++i){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        vector<int>path(numCourses,0);
-        stack<int>st; //for topo sort
+        //Making the connections for a directed graph siu
+        
         for(int i=0;i<numCourses;++i){
-            //multiple components siu
-            if(visited[i]==0 && cycle(i,adj,path,visited,st)){
+            if(visited[i]==0 && isCycle(i,adj,visited,path,st)){
                 return {};
             }
         }
-        
         while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
         }
-        
+
         return ans;
     }
 };
