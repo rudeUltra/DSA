ab85b01ac02ee4d590843758dabc7bef91dade26
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-18 15:45:36 +0530

    Time: 14 ms (88.54%), Space: 13.8 MB (64.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>adj[],vector<int>&visited,vector<int>&path){
+    
+    bool isCycle(int node,vector<int>adj[],vector<int>&visited,vector<int>&path){
         visited[node]=1;
         path[node]=1;
         
-        for(auto it : adj[node]){
-            if(visited[it]==0){
-                if(dfs(it,adj,visited,path)==1){
-                    return true;
-                }
+        for(auto it:adj[node]){
+            if(visited[it]==0 && isCycle(it,adj,visited,path)){
+                return true;
             }
-            else if(visited[it]==1 && path[it]==1){
+            else if(visited[it]==path[it]){
+                //SUS
                 return true;
             }
         }
-        
         path[node]=0;
         return false;
     }
     
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        //directed graph cycle detection sIUU
-        int n=prerequisites.size();
-        vector<int>adj[numCourses];
-        
-        for(int i=0;i<n;++i){
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-        }
-        
         vector<int>visited(numCourses,0);
+        vector<int>adj[numCourses];
         vector<int>path(numCourses,0);
+        for(int i=0;i<prerequisites.size();++i){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        //Making the connections for a directed graph siu
+        
         for(int i=0;i<numCourses;++i){
-            if(visited[i]==0){
-                if(dfs(i,adj,visited,path)==1){
-                    return false;
-                }
+            if(visited[i]==0 && isCycle(i,adj,visited,path)){
+                return false;
             }
         }
-        
-        return true; //no cycle siu
-    
+        return true;
     }
 };
