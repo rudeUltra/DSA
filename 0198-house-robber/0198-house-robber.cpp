f7fdda5c43e372bc47c5a3cb19f3a48a47017551
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-19 12:15:09 +0530

    Time: 4 ms (24.44%), Space: 8 MB (8.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     
+    
     int f(int idx,vector<int>&nums,vector<int>&dp){
+        if(idx==nums.size()-1){
+            //Last house so rob it
+            return nums[idx];
+        }
         if(idx>=nums.size()){
-            return 0; //cant rob anymore siu
+            return 0;
         }
         if(dp[idx]!=-1){
-            return dp[idx];
+            return dp[idx]; //Memoize
         }
-        int notrob=f(idx+1,nums,dp);
-        int rob=f(idx+2,nums,dp)+nums[idx];
         
-        return dp[idx]=max(rob,notrob);
+        int notpick=f(idx+1,nums,dp);
+        int pick=f(idx+2,nums,dp)+nums[idx];
         
+        return dp[idx]=max(notpick,pick);
     }
-    
-    
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n+1,-1); //N+1 possible states
+        
+        //At this index what is the max profit i can get siu
         
         return f(0,nums,dp);
     }
