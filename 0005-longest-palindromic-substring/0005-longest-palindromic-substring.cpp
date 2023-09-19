d79356215995345b114231dad4b5f13cb96f4097
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-19 16:24:38 +0530

    Time: 54 ms (57.21%), Space: 159.9 MB (22.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,39 +1,38 @@
 class Solution {
-private: 
-    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-        if(i == j){
-            return dp[i][j] = true;
-        }
-        if(j-i == 1){
-            if(s[i] == s[j]){
-                return dp[i][j] = true;
+public:
+    int mx=1;
+    void palindrome(string s,int i,int j,int &left,int &right){
+        while(i>=0 && j<s.size() && s[i]==s[j]){
+            if(j-i+1>mx){
+                mx=j-i+1;
+                left=i;
+                right=j;
+                
             }
-            else{
-                return dp[i][j] = false;
-            }
-        }
-        if(s[i] == s[j] && dp[i+1][j-1] == true){
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
+            --i;
+            j++;
         }
     }
-public:
+    
+    
     string longestPalindrome(string s) {
-        int n = s.size();
-        int startIndex = 0; int maxlen = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int g=0; g<n; g++){
-            for(int i=0, j=g; j<n; i++, j++){
-                solve(dp, i, j, s);
-                if(dp[i][j] == true){
-                    if(j-i+1 > maxlen){
-                        startIndex = i;
-                        maxlen = j-i+1;
-                    }
-                }
-            }
+        int n=s.size();
+        string ans="";
+        int left=0;
+        int right=0;
+        
+        
+        for(int i=0;i<n;++i){
+            //odd case
+            palindrome(s,i,i,left,right);
+            palindrome(s,i,i+1,left,right);
         }
-        return s.substr(startIndex, maxlen);
+        
+        ans=s.substr(left,mx);
+        
+        return ans;
+        
+        
+        
     }
 };
