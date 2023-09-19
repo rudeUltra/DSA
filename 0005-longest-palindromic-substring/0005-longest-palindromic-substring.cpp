5e2345afe2951434d76416bda1d24e69d9fabcb9
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-19 15:14:31 +0530

    Time: 172 ms (39.79%), Space: 23 MB (31.30%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,51 +1,49 @@
+//bottom-up approach
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.length();
-        if(n<=0){
-            return "";
-        }
-        if(n==1){
-            return s;
-        }
-        int start=0;
-        int end=0;
-        int l=0,r=0;
-        for(int i=0;i<n;++i){
-            //two conditions even palindrome and odd palindrome
-            l=i;
-            r=i+1;
-            
-            //odd case normal
-            while(l>=0 && r<n && s[l]==s[r]){
-                //check for new max lenght 
-                if(r-l>end-start){
-                    start=l;
-                    end=r;
-                }
-                l--;
-                r++;
+        int n = s.size();
+        int start = 0; // Starting index of the longest palindromic substring
+        int longlen = 1; // Length of the longest palindromic substring (initialize to 1, as each character is a palindrome by itself)
+        
+        // Create a table to store the results of subproblems
+        vector<vector<bool>> t(n, vector<bool>(n, false));
+        
+        // All substrings of length 1 are palindromic
+        for (int i = 0; i < n; i++)
+            t[i][i] = true;
+        
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; i++) 
+        {
+            if (s[i] == s[i + 1])
+            {
+                t[i][i+1] = true;
+                start = i;
+                longlen = 2;
             }
-            
-            //even case a-c-a
-            
-            l=i;
-            r=i+2; //ignore middle guy
-              while(l>=0 && r<n && s[l]==s[r]){
-                //check for new max lenght 
-                if(r-l>end-start){
-                    start=l;
-                    end=r;
+        }
+        
+        // Check for substrings of length 3 or more
+        for (int l = 3; l <= n; l++) 
+        {
+            for (int i = 0; i < n - l + 1; i++) 
+            {
+                int j = i + l - 1; // Ending index of the current substring
+                // Check if the substring from i to j is a palindrome
+                if (s[i] == s[j] && t[i + 1][j - 1]) 
+                {
+                    t[i][j] = true;
+                    if (l > longlen) 
+                    {
+                        longlen = l;
+                        start = i;
+                    }
                 }
-                l--;
-                r++;
             }
-            
         }
-        string temp;
-        for(int i=start;i<=end;++i){
-            temp.push_back(s[i]);
-        }
-        return temp; //less go
+        
+        //Extract the longest palindromic substring from original string
+        return s.substr(start, longlen);
     }
 };
