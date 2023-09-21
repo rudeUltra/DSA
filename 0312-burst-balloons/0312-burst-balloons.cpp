b7fcb7ee03431cf12db9235de52c15ef21e49cbc
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-09-21 15:50:36 +0530

    Time: 463 ms (21.24%), Space: 10.7 MB (5.13%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int maxCoins(vector<int>& nums) {
-        int n=nums.size();
-        nums.insert(nums.begin(),1);
-        nums.push_back(1); // for out of bounds condition
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return f(1,n,nums,dp);
-    }
-     int f(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
-        if(i>j) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int res=INT_MIN;
-        for(int k=i;k<=j;k++){
-            
-            int ans=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums,dp)+f(k+1,j,nums,dp);
-            res=max(res,ans);
+    int f(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
+        if(i>j){
+            return 0; //NO more balloons sed
+        }
+        int ans=0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        for(int k=i;k<=j;++k){
+            //Cut
+            int cost=nums[i-1]*nums[k]*nums[j+1];
+            int temp=f(i,k-1,nums,dp)+f(k+1,j,nums,dp)+cost;
+            ans=max(ans,temp);
         }
         
-        return dp[i][j]=res;
+        return dp[i][j]=ans;
     }
     
-   
     
+    int maxCoins(vector<int>& nums) {
+        //for easy and creating Indepented subproblems imagine u are bursting the current balloon last siu
+        int n=nums.size();
+        nums.push_back(1); //boundary
+        nums.insert(nums.begin(),1);
+        
+        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
+        
+        return f(1,n,nums,dp);
+    }
 };
