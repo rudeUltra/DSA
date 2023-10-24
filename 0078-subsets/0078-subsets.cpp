4766d8734e7e7148799fe6d4a12dee24844f00b0
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-10-24 15:35:53 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (51.98%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    
-    void f(int idx,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
-        if(idx==nums.size()){
-            ans.push_back(temp);
-            return;
-        }
-        f(idx+1,nums,temp,ans);
-        temp.push_back(nums[idx]);
-        f(idx+1,nums,temp,ans);
-        temp.pop_back();
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        //Siu
-        //Basically pick or not pick siu
-        vector<int>temp;
-        vector<vector<int>>ans;
-        //It will reach the end xD
-        f(0,nums,temp,ans);
+        int n=nums.size();
         
+        vector<vector<int>>ans;
+        
+        for(int i=0;i<(1<<n);++i){
+            vector<int>temp;
+           
+            
+            for(int j=0;j<n;++j){
+                
+                
+                if(i&(1<<j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            for(auto it:temp){
+                cout<<it<<" ";
+            }
+            cout<<endl;
+            ans.push_back(temp);
+        }
         return ans;
+        
     }
 };
