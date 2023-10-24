bf48c465d5f81952d6070c5db54f4551bec40902
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-10-24 15:20:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (46.03%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -3,19 +3,15 @@ public:
     int minBitFlips(int start, int goal) {
         int count=0;
         
-        for(int i=0;i<32;++i){
+        for(int i=31;i>=0;--i){
+            int start_bit=(start>>i)&1;
+            int goal_bit=(goal>>i)&1;
             
-            cout<<(goal&1)<<" ";
-            
-            if((start&1)!=(goal&1)){
+            if(start_bit!=goal_bit){
                 count++;
             }
             
-            start=start>>1;
-            goal=goal>>1;
-            
         }
-        
         return count;
     }
 };
