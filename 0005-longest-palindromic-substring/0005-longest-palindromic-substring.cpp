585b24d8246968f48206e86268916c8f16de3d2f
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-10-27 11:39:38 +0530

    Time: 10 ms (86.21%), Space: 7.2 MB (86.68%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int mx=1;
-    void palindrome(string s,int i,int j,int &left,int &right){
-        while(i>=0 && j<s.size() && s[i]==s[j]){
-            if(j-i+1>mx){
-                mx=j-i+1;
-                left=i;
-                right=j;
-                
+    
+    void palindrome(string &s,int l,int r,int &left,int &right,int &mx){
+        int n=s.size();
+        
+        while(l>=0 && r<n){
+            if(s[l]!=s[r]){
+                break;
             }
-            --i;
-            j++;
+            if((r-l+1>mx)){
+                mx=r-l+1;
+                left=l;
+                right=r;
+            }
+            l--;
+            r++;
         }
     }
-    
-    
     string longestPalindrome(string s) {
+        //Siuu
         int n=s.size();
         string ans="";
         int left=0;
         int right=0;
-        
-        
+        int mx=INT_MIN;
         for(int i=0;i<n;++i){
-            //odd case
-            palindrome(s,i,i,left,right);
-            palindrome(s,i,i+1,left,right);
+            palindrome(s,i,i+1,left,right,mx);
+            palindrome(s,i,i,left,right,mx);
+            
         }
         
-        ans=s.substr(left,mx);
+        ans=s.substr(left,right-left+1);
         
         return ans;
-        
-        
-        
     }
 };
