cd265305b3d9493e74da6fb7730165c2ac12766b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-11-24 14:54:52 +0530

    Time: 82 ms (95.28%), Space: 24.3 MB (82.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        //not a hashmap problem siu
         int n=nums.size();
+        
         vector<vector<int>>ans;
+        
         sort(nums.begin(),nums.end());
         
-        //3 sum so less than n-1 siu
-        
-        for(int i=0;i<n-2;++i){
-            //target is 0 siuu
+        for(int i=0;i<nums.size()-2;++i){
+            //I is fixed now find the rest with two pointer approach siu
             if(i>0 && nums[i]==nums[i-1]){
-                continue; //avoid duplicates bc
+                continue; //Duplicate
             }
-            //2 pointer to find sum siu
-            int target=-nums[i];
+            
             int left=i+1;
             int right=n-1;
             
+            int target=-nums[i];
+            
             while(left<right){
-                if(nums[left]+nums[right]==target){
-                    //wooow 
-                    //but duplicates betichod
+                int sum=nums[left]+nums[right];
+                
+                if(sum<target){
+                    left++;
+                }
+                else if(sum>target){
+                    right--;
+                }
+                else{
+                    //gotemm
                     ans.push_back({nums[i],nums[left],nums[right]});
+                    
+                    //now take care of duplicates 
+                    
                     while(left<n-1 && nums[left]==nums[left+1]){
                         left++;
                     }
                     while(right>0 && nums[right]==nums[right-1]){
                         right--;
                     }
-                    left++;
                     right--;
-                }
-                else if(nums[left]+nums[right]<target){
-                    //chota sed
                     left++;
-                }
-                else{
-                    right--;
                 }
             }
+            
+           
         }
-        
-        return ans;
+         return ans;
     }
 };
