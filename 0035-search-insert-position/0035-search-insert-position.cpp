1c27cca1fa1b147a629675e09e6b30f4a1c66805
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-11-25 14:59:12 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (14.38%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        //lets check low and mid value HMM
-        int n=nums.size(); //binary search obvs
+        int n=nums.size();
+        
         int low=0;
         int high=n-1;
+        
         while(low<=high){
-            int mid=low+(high-low)/2;
-
+            int mid=(low+(high-low)/2);
+            
             if(nums[mid]==target){
-                //found siu
                 return mid;
             }
             else if(nums[mid]>target){
-                //kam karo bhai
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
+            
         }
-
+        
         return low;
     }
 };
