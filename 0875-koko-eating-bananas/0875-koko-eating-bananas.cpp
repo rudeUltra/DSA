f1563971ae25cc7334c84d41c0c885d681168220
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-11-25 15:28:46 +0530

    Time: 52 ms (7.10%), Space: 19.2 MB (69.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    bool isvalid(int k,vector<int>&piles,int h){
-        long long count=0; //days
+    
+    bool isSafe(vector<int>&piles,int target,int mid){
+        long long count=0;
+        
         for(int i=0;i<piles.size();++i){
-            if(piles[i]<=k){
+            
+            if(piles[i]<mid){
                 count++;
             }
             else{
-                count+=piles[i]/k;
-                if(piles[i]%k!=0){
+                count+=piles[i]/mid;
+                if(piles[i]%mid){
                     count++;
                 }
             }
         }
-        
-        return count<=h;
+        return count<=target;
     }
-    
-    
     
     int minEatingSpeed(vector<int>& piles, int h) {
         int n=piles.size();
         int mx=INT_MIN;
+        
         for(int i=0;i<n;++i){
             mx=max(mx,piles[i]);
         }
         
         int low=1;
         int high=mx;
+        
         int ans=0;
         while(low<=high){
-            //siu
             int mid=low+(high-low)/2;
+          
             
-            if(isvalid(mid,piles,h)){
+            if(isSafe(piles,h,mid)){
                 ans=mid;
                 high=mid-1;
             }
@@ -41,7 +43,7 @@ public:
                 low=mid+1;
             }
         }
-        
         return ans;
+        
     }
 };
