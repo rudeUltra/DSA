865afdd89d07ae4ac9eed2681918a3eaadf6b13a
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-03 15:50:25 +0530

    Time: 57 ms (53.14%), Space: 41.8 MB (57.21%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,52 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+      
+	return async function(...args) {
+        
+       const promise1=fn(...args);
+        
+        const promise2=new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                reject("Time Limit Exceeded")
+            },t);
+        });
+        
+        return Promise.race([promise1,promise2]);
+        
+        
+        
+        
+        
+        
+    }
+};
+
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+// var timeLimit = function(fn, t) {
+// 	return async function(...args) {
+//            const originalPromise=fn(...args);
+//            const rejectPromise=new Promise((resolve,reject)=>{setTimeout(()=>{
+//                reject("Time Limit Exceeded")
+//            },t);
+//            });
+//           return Promise.race([originalPromise,rejectPromise]);  
+//     }
+// };
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
