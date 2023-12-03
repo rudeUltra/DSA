8395d34a1e0e173310d71517c5ffefdfcb1e7aaf
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-03 19:32:00 +0530

    Time: 67 ms (62.84%), Space: 42.1 MB (73.09%) - LeetHub

---
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,25 @@
+var promiseAll = async function(functions) {
+    return new Promise((resolve,reject) => {
+        if(functions.length === []) {
+            resolve([]);
+            return;
+        }
+        
+        const res = new Array(functions.length).fill(null);
+
+        let resolvedCount = 0;
+
+        functions.forEach(async (el,idx) => {
+            try {
+                const subResult = await el();
+                res[idx] = subResult;
+                resolvedCount++;
+                if(resolvedCount=== functions.length) {
+                    resolve(res);
+                }
+            } catch(err) {
+                reject(err);
+            }
+        });
+    });
+};
