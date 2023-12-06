06e4e401d157d817deaf1e7ef1968b0f6d39b412
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-07 00:19:19 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (59.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)return 1;
-        if(n==1)return x;
-        if(n>0){
-        if(n%2==0)return myPow(x*x,n/2);
-        else return x*myPow(x*x,n/2);
+        //This concept has been in my head for a long time damn
+        if(n==0){
+            return 1;
         }
-        else{
-            n=abs(n);
-            if(n%2==0)return 1/(myPow(x*x,n/2)); //for negative just do 1/pow siu
-        else return 1/(x*myPow(x*x,n/2));
-       
+        if(n==1){
+            return x;
+        }
+        if(n>0){
+            if(n%2==0){
+                return myPow(x*x,n/2);
+            }
+            else{
+                return x*myPow(x*x,n/2);
+            }
         }
         
+        n=abs(n);
+        if(n%2==0){
+                return 1/(myPow(x*x,n/2));
+            }
+    
+        return 1/(x*myPow(x*x,n/2));
+        
+        
+        //Negative part
     }
 };
