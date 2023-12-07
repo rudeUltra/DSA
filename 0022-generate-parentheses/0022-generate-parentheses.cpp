4cdd29b8ca26830968ced07b169c7f8531f0e2ae
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-07 14:23:42 +0530

    Time: 3 ms (65.36%), Space: 15.2 MB (61.43%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    void f(int idx,int n,int open,int close,string temp,vector<string>&ans){
-        if(idx==2*n){
+    
+    void f(int open,int close,int n,vector<string>&ans,string temp){
+        if(open==n && close==n){
+            //done 
             ans.push_back(temp);
             return;
         }
-        if(open<n){
-            //hm
-            f(idx+1,n,open+1,close,temp+'(',ans);
-        }
         
+        if(open<n){
+            f(open+1,close,n,ans,temp+'(');
+        }
         if(close<open){
-            f(idx+1,n,open,close+1,temp+')',ans);
+            f(open,close+1,n,ans,temp+')');
         }
     }
-    
     vector<string> generateParenthesis(int n) {
-        //2 interesting observation edge case + decision
-        string temp;
+        //Decision making 
+        //Drop rain hm
         vector<string>ans;
-        
-        f(0,n,0,0,temp,ans);
+        f(0,0,n,ans,"");
         
         return ans;
     }
-    
-    
 };
