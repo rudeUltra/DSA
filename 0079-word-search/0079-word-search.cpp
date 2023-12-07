88da1cfc5e030f9d427b22be62371ec1695f700a
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-07 15:52:48 +0530

    Time: 308 ms (78.36%), Space: 8.6 MB (25.15%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,88 +1,54 @@
 class Solution {
 public:
     
-   
     
-    
-// bool dfs(int i,int j,vector<vector<char>>&board,string &word,int k,int n,int m,vector<vector<int>>&visited){
-//     if(k==word.size()){
-//         return true;
-//     }
-//     if(i<0 || i>=n || j<0 || j>=m || board[i][j]!=word[k] || visited[i][j]==1){
-//         return false;
-//     }
-
-//     visited[i][j]=1;
-//     bool x=dfs(i-1,j,board,word,k+1,n,m,visited);
-//     bool y=dfs(i+1,j,board,word,k+1,n,m,visited);
-//     bool z=dfs(i,j-1,board,word,k+1,n,m,visited);
-//     bool b=dfs(i,j+1,board,word,k+1,n,m,visited);
-
-//     if(x || y || z || b){
-//         return true;
-//     }
-//     visited[i][j]=0;
-//     return false;
-
-// }
-    
-    
-    bool dfs(int i,int j,vector<vector<char>>&board,string &word,int k,vector<vector<int>>&visited){
+    bool dfs(int i,int j,vector<vector<char>>&board,string &word,vector<vector<char>>&vis,int k){
         if(k==word.size()){
-            return true;
+            return true; //gotemm
         }
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[k] || visited[i][j]==1){
+        
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || vis[i][j]==1 || board[i][j]!=word[k]){
             return false;
         }
-        visited[i][j]=1;
         
-        bool x=dfs(i+1,j,board,word,k+1,visited);
-        bool y=dfs(i-1,j,board,word,k+1,visited);
-        bool z=dfs(i,j+1,board,word,k+1,visited);
-        bool b=dfs(i,j-1,board,word,k+1,visited);
+        vis[i][j]=1;
+        bool ans=false;
+        ans=ans | dfs(i+1,j,board,word,vis,k+1);
+        ans=ans |dfs(i-1,j,board,word,vis,k+1);
+        ans=ans | dfs(i,j+1,board,word,vis,k+1);
+        ans=ans | dfs(i,j-1,board,word,vis,k+1);
         
-        if(x || y || z || b){
-            return true;
-        }
-        
-        visited[i][j]=0;
         //backtrack
-        return false;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    bool exist(vector<vector<char>>& board, string word) {
-        //So we have to make sure of the basic backtracking process do the possibilites then backtrack and undo it
-        //Same char can be used in a different way in another dfs call so we have to big brain and unmark it siu
+        vis[i][j]=0;
         
-        //DFS + Backtracking
+        return ans;
+        
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        
+        //DFS + Backtrack f
+        //Let's do the Harder DP list 
+        
+        //visit if no success then trackback siu
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-       
+        
+     
+        
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(board[i][j]==word[0]){
-                    
-                    if(dfs(i,j,board,word,0,dp)==1){
+                    vector<vector<char>>vis(n+1,vector<char>(m+1,-1));
+                    if(dfs(i,j,board,word,vis,0)){
                         return true;
                     }
-                    
                 }
             }
         }
         
-        //sed 
-        return false;
+        return false; //sEd \U0001f614
+        
+        
     }
 };
