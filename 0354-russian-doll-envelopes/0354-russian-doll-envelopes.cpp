24d41df39cafa025d3c7adbe762f2be9a14c9f2b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-20 13:57:55 +0530

    Time: 303 ms (38.12%), Space: 79 MB (33.28%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-   static bool cmp(vector<int>& a, vector<int>& b)
-{
-    if(a[0]==b[0]) return a[1] > b[1];
-    return a[0] < b[0];
-}
- int solveOptimal(vector<vector<int>>& arr) {
-        if(arr.size() == 0)
-            return 0;
-        vector<int> ans;
-        ans.push_back(arr[0][1]);
-
-        for(int i=1; i<arr.size(); i++) {
-            if(arr[i][1]> ans.back()) {
-                //include
-                ans.push_back(arr[i][1]);
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        
+        return a[0]<b[0];
+        
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        
+        //So scared to fail that i stopped writing code damn.
+        //fail a million times idc.
+        //Knowledge is priceless 
+          int n=envelopes.size();
+        
+        sort(envelopes.begin(),envelopes.end(),cmp);
+        
+       
+        
+        vector<int>lis(n);
+        for(int i=0;i<n;++i){
+            lis[i]=(envelopes[i][1]);
+        }
+        
+        
+        vector<int>ans;
+        
+        ans.push_back(lis[0]);
+        
+        for(int i=1;i<n;++i){
+           
+            if(lis[i]>ans.back()){
+                ans.push_back(lis[i]);
             }
-            else {
-                //overwrite
-                //find index of just bada element
-                int index = lower_bound(ans.begin(), ans.end(), arr[i][1]) - ans.begin();
-                ans[index] = arr[i][1];
+            else{
+                int idx=lower_bound(ans.begin(),ans.end(),lis[i])-ans.begin();
+                ans[idx]=lis[i];
             }
         }
+        
         return ans.size();
-    }
-
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-       
-        sort(envelopes.begin(), envelopes.end(),cmp);
-
-        //apply lis logic
-        int ans = solveOptimal(envelopes);
-        return ans;
+        
     }
 };
