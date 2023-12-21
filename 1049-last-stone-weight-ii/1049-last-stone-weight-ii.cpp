89b0681822914ef7915248d0d2be5cb3828d1fa3
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-21 13:09:26 +0530

    Time: 10 ms (29.90%), Space: 9.8 MB (40.20%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
         int n=stones.size();
-        int s=0;
-        for(int i=0;i<n;i++){
-            s+=stones[i];
+        
+        int sum=0;
+        
+        for(int i=0;i<n;++i){
+            sum+=stones[i];
         }
-        int maxs=0;
-        vector<vector<bool>> dp(n+1,vector<bool> (s/2+1,false));
-        dp[0][0]=true;
+        int x=(sum/2)+1;
+        vector<vector<int>>dp(n+1,vector<int>(x,0));
+        int target=0;
         for(int i=0;i<n+1;i++){
-            dp[i][0]=true;
+            dp[i][0]=1;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=s/2;j++){
-                dp[i][j]=dp[i-1][j];
+        for(int i=1;i<=n;++i){
+            for(int j=0;j<=sum/2;++j){
+                //two option pick or no pick
+                
+                dp[i][j]=dp[i-1][j]; //not pick
+                
                 if(j>=stones[i-1]){
-                    dp[i][j]=dp[i-1][j] || dp[i-1][j-stones[i-1]];
+                    dp[i][j]=dp[i-1][j] | dp[i-1][j-stones[i-1]];
                 }
-                if(dp[i][j]==true){
-                    maxs=max(maxs,j);
+                
+                if(dp[i][j]){
+                    //Maximum possible sum hm
+                    target=max(target,j);
                 }
             }
         }
-        return s-2*maxs;
+        
+        return sum-2*target;
+        
         
     }
 };
