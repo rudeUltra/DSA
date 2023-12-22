40ba73c12948f9467a4aee1a420171da7897cb9e
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2023-12-22 17:32:42 +0530

    Time: 12 ms (6.03%), Space: 8.9 MB (25.22%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int func(vector<int>& a, int i, int j, vector<vector<int>>& dp)
-    {
-        if(j<=(i+1))
+    
+    int f(int i,int j,vector<int>&values,vector<vector<int>>&dp){
+        if(i==j || i>j || i+1==j){
+            //we need minimum of 3 points hm
             return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int ans=INT_MAX;
-        for(int k=i+1;k<j;k++)
-        {
-            int temp = (a[i]*a[j]*a[k]) + func(a,i,k,dp) + func(a,k,j,dp);
-            ans = min(ans,temp);
         }
-        return dp[i][j] = ans;
+        
+        
+        //
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=INT_MAX;
+        
+        for(int k=i+1;k<j;++k){
+            int cut=values[i]*values[k]*values[j];
+            
+            ans=min(ans,f(i,k,values,dp)+f(k,j,values,dp)+cut);
+        }
+        
+        
+        //Wow first attempt noice noice
+        return dp[i][j]=ans;
     }
     
-    int minScoreTriangulation(vector<int>& a) {
-        int n=a.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
-        int ans = func(a,0,n-1,dp);
-        return ans;
+    
+    
+    int minScoreTriangulation(vector<int>& values) {
+        int n=values.size();
+        //i-j subproblem hm
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        
+        //Choose first and last as the base points for a trianle big brain hm
+        
+        return f(0,n-1,values,dp);
+        
     }
 };
