1fe0d000c6593e2c515f98167bf38d9c5e224ac8
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-03 12:47:36 +0530

    Time: 120 ms (11.76%), Space: 29.1 MB (29.62%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int n=bank.size();
-        int m=bank[0].size();
+        int prev = 0, ans = 0;
         
-        vector<int>rows(n,0);
-        
-        for(int i=0;i<n;++i){
-            int count=0;
-            for(int j=0;j<m;++j){
-                if(bank[i][j]=='1'){
+        for (string s : bank) {
+            int count = 0;
+            for (char c : s) {
+                if (c == '1') {
                     count++;
                 }
             }
-            rows[i]=count;
-            cout<<rows[i]<<" ";
-        }
-        
-        int ans=0;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(bank[i][j]=='1'){
-                    
-                    int count=0;
-                    int k=i+1;
-                    
-                     if(k==n){
-                        break;
-                    }
-                    
-                    //go down the rows hm
-                    while(k<n && rows[k]==0){
-                        k++;
-                    }
-                    
-                     if(k==n){
-                        break;
-                    }
-                    
-                    count+=rows[k];
-                    ans+=count;
-                }
+            if (count != 0) {
+                ans += (prev * count);
+                prev = count;
             }
         }
         
         return ans;
     }
 };
+
+//Dont always simulate use little maths slash smort also
