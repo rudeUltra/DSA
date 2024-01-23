05ca817771e3859a039c9e44b838776bf81b7aa0
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-23 22:49:04 +0530

    Time: 4 ms (39.99%), Space: 8.3 MB (48.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        //Pick not pick or All combinations hm interesting
-        int n=nums.size();
+        //Interesting
+        
         vector<vector<int>>ans;
+        int n=nums.size();
         
         for(int i=0;i<(1<<n);++i){
+            //2 to the power n hm
             vector<int>temp;
             for(int j=0;j<n;++j){
                 if(i&(1<<j)){
-                    //this bit is set siu
                     temp.push_back(nums[j]);
                 }
             }
-            
             ans.push_back(temp);
         }
-        
         return ans;
     }
 };
