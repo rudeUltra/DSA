463646b710b9d0ae4137ca782f8b6e119d29dca4
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-23 22:38:38 +0530

    Time: 8 ms (36.42%), Space: 7.2 MB (9.22%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         int count=0;
-        
-        for(int i=31;i>=0;--i){
+         
+
+            
+        for(int i=0;i<32;++i){
+            //INT are 32 bits sized hm
             int start_bit=(start>>i)&1;
             int goal_bit=(goal>>i)&1;
             
-            if(start_bit!=goal_bit){
-                count++;
+            if(i<5){
+                cout<<start_bit<<" "<<goal_bit<<" ";
+            }
+            if(start_bit ^ goal_bit){
+                count++; //DIFFerent siu
             }
             
+            //xOR MEANS different hm
         }
+        
         return count;
     }
 };
