4197ae5ae6393a69186fb04143a37fbf004e8dcd
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-26 15:37:26 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (22.69%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    
     const int mod=1e9+7;
-    int f(int i,int j,int limit,int n,int m,vector<vector<vector<int>>>&dp){
-        if(limit<0){
-            return 0; //sed
+    
+    int f(int n,int m,int maxMove,int i,int j,vector<vector<vector<int>>>&dp){
+        
+        if(maxMove<0){
+            return 0; //Sed
         }
         
         if(i<0 || i>=n || j<0 || j>=m){
-            return 1; //gotemm
+            return 1;
         }
         
-        if(dp[i][j][limit]!=-1){
-            return dp[i][j][limit];
+        if(dp[i][j][maxMove]!=-1){
+            return dp[i][j][maxMove];
         }
         
-        int down=f(i+1,j,limit-1,n,m,dp)%mod;
-        int up=f(i-1,j,limit-1,n,m,dp)%mod;
-        int right=f(i,j+1,limit-1,n,m,dp)%mod;
-        int left=f(i,j-1,limit-1,n,m,dp)%mod;
+        //check 4 directions hm.
         
-        int ans=(down+up)%mod;
-        ans+=(right+left)%mod;
-        return dp[i][j][limit]=ans%mod; //Hm
+        int ans=0;
+        
+        ans=(ans+f(n,m,maxMove-1,i+1,j,dp))%mod;
+        ans=(ans+f(n,m,maxMove-1,i-1,j,dp))%mod;
+        ans=(ans+f(n,m,maxMove-1,i,j+1,dp))%mod;
+        ans=(ans+f(n,m,maxMove-1,i,j-1,dp))%mod;
+        
+        return dp[i][j][maxMove]=ans%mod;
     }
     
+    
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        //interesting hm
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(maxMove+1,-1)));
-        return f(startRow,startColumn,maxMove,m,n,dp);
+        //Try out all paths hm
+        
+        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(maxMove+1,-1)));
+        
+        //3d dp hm
+        
+        return f(m,n,maxMove,startRow,startColumn,dp);
     }
 };
