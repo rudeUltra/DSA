efc437ef3d2fccf34d8134b21f45e8bbf1093985
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-29 22:10:46 +0530

    Time: 556 ms (90.94%), Space: 182.8 MB (33.22%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,76 +1,97 @@
-struct Node{
-    Node *links[2]; //0 and 1 siu
-
-    Node * get(int c){
-        return links[c];
+class Node{
+    public:
+    
+    Node*links[2];
+    
+    Node(){
+        links[0]=NULL;
+        links[1]=NULL;
     }
-    bool isSet(int c){
-        if(links[c]==NULL){
+    
+    bool bitSet(int bit){
+        if(links[bit]==NULL){
             return false;
         }
         return true;
     }
-    void set(int c,Node* node){
-        links[c]=node;
+    
+    void addBit(int bit){
+        Node *temp=new Node();
+        links[bit]=temp;
+    }
+    
+    Node*next(int bit){
+        return links[bit];
     }
 };
+
+
+
 class Trie{
     public:
-    Node *root;
+    
+    Node* root;
+    
     Trie(){
-        root=new Node(); //sIUUU
+        root=new Node();
     }
-    void insert(int num){
-        Node *curr=root;
+    
+    void push(int num){
+        Node*curr=root; //POINT TO THE root hm
+        
         for(int i=31;i>=0;--i){
-            int set=(num>>i)&1;
-            //Right shift because we only want 1 no more Big brain
-            
-            if(!curr->isSet(set)){
-                curr->set(set,new Node());
+            int bit=(num>>i)&1;
+            if(curr->bitSet(bit)==false){
+                curr->addBit(bit);
             }
-            curr=curr->get(set);
+            curr=curr->next(bit);
         }
+        //added hm
     }
-    int getmax(int num){
-        //just like searching SIUu
-        Node *curr=root;
+    
+    int getMax(int num){
+        Node*curr=root;
+        
         int ans=0;
+        
         for(int i=31;i>=0;--i){
-            int set=(num>>i)&1;
-            //there will always be a reference node since 31 numbers bruh
-            // if opposite exists then add it and move on 
-            if(curr->isSet(!set)){
-                //siuu
-                ans=ans | (1<<i); //building the ans; Left shift here because we want to add the number bruh
-                curr=curr->get(!set);
+            int bit=(num>>i)&1;
+            
+            //check if opposite bit possible or not siu
+            if(curr->bitSet(!bit)==true){
+                //gotemm
+                ans=ans | (1<<i);
+                curr=curr->next(!bit); //track the trie hm.
             }
             else{
-                curr=curr->get(set);
+                curr=curr->next(bit);
             }
+            
         }
+        
         return ans;
     }
 };
 
 
-
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        //sai raam sighs
+        //It's doing Nx32 hm noice
         int n=nums.size();
-        vector<int>temp=nums;
-        Trie t;
-        int mx=INT_MIN;
+        
+        Trie* root=new Trie();
+        
         for(int i=0;i<n;++i){
-            t.insert(nums[i]);
+            root->push(nums[i]);
         }
+        
+        int ans=0;
+        
         for(int i=0;i<n;++i){
-            mx=max(mx,t.getmax(temp[i]));
+            ans=max(ans,root->getMax(nums[i]));
         }
-
-        return mx;
-
+        
+        return ans;
     }
 };
