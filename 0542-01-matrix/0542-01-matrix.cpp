ee9adc00ee9a2f4409708c3ffe500541fb387e62
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-30 16:04:31 +0530

    Time: 57 ms (80.38%), Space: 33.8 MB (60.48%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,48 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        //Bhai bayern Come on...
+        //Go Destination to source siu
         int n=mat.size();
         int m=mat[0].size();
         
-        //should i put 1 or 0 in the matrix hm
-        vector<vector<int>>dist(n,vector<int>(m,1e5));
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         
-        //basically can i help plus ok i helped this guy can this guy help others siu
         queue<pair<int,int>>pq;
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(mat[i][j]==0){
+                    //push
                     pq.push({i,j});
                     dist[i][j]=0;
                 }
+                
             }
         }
-        vector<int>x{-1,0,1,0};
-        vector<int>y{0,1,0,-1};
-        //multiple starting points hm siu
+        
+        //Bhaiiii god gave another lesson dont give 
+        
+       int dx[]={-1,0,1,0};
+       int dy[]={0,1,0,-1};
+        
         while(!pq.empty()){
+            //No need to do level order
             auto it=pq.front();
             pq.pop();
             
-            //four directions
-            int x1=it.first;
-            int y1=it.second;
+            //CHeck 4 direction and see where can i help
             
             for(int i=0;i<4;++i){
-                int new_x=x1+x[i];
-                int new_y=y1+y[i];
+                int new_x=it.first+dx[i];
+                int new_y=it.second+dy[i];
                 
                 if(new_x>=0 && new_x<n && new_y>=0 && new_y<m){
-                    if(dist[x1][y1]+1<dist[new_x][new_y]){
-                        //I can help siu
-                        dist[new_x][new_y]=1+dist[x1][y1];
+                    if(dist[it.first][it.second]+1<dist[new_x][new_y]){
+                        //Aha
+                        dist[new_x][new_y]=1+dist[it.first][it.second];
                         pq.push({new_x,new_y});
                     }
                 }
             }
+            
         }
         
+        
         return dist;
+        
+        
         
     }
 };
