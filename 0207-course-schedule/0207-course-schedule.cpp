ad95b4fcb043c902f88c59717e77e1769a77a15b
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-30 18:42:12 +0530

    Time: 20 ms (49.00%), Space: 16.8 MB (22.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -6,33 +6,44 @@ public:
         path[node]=1;
         
         for(auto it:adj[node]){
-            if(visited[it]==0 && isCycle(it,adj,visited,path)){
-                return true;
+            if(visited[it]==0){
+                //aha
+                if(isCycle(it,adj,visited,path)){
+                    return true;
+                }
             }
-            else if(visited[it]==path[it]){
-                //SUS
+            else if(visited[it]==1 && path[it]==1){
+                //SAME cycle BC.
                 return true;
             }
         }
+        
         path[node]=0;
+        
         return false;
     }
     
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>visited(numCourses,0);
         vector<int>adj[numCourses];
-        vector<int>path(numCourses,0);
+        
         for(int i=0;i<prerequisites.size();++i){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        //Making the connections for a directed graph siu
+        
+        vector<int>visited(numCourses,0);
+        vector<int>path(numCourses,0);
         
         for(int i=0;i<numCourses;++i){
-            if(visited[i]==0 && isCycle(i,adj,visited,path)){
-                return false;
+            if(visited[i]==0){
+                //hm potential noice
+                if(isCycle(i,adj,visited,path)){
+                    return false;
+                }
             }
         }
-        return true;
+        
+        return true; //nO Cycles Gotemm
+        
     }
 };
