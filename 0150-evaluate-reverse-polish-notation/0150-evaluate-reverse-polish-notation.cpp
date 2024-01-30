e33c6109b6dd16fadc07aee8f3a859a93d906609
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-30 14:47:55 +0530

    Time: 4 ms (94.03%), Space: 15.5 MB (10.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
+    bool isOperand(string c){
+        if(c=="+" || c=="-" || c=="/" || c=="*"){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
+    
     int evalRPN(vector<string>& tokens) {
-        //its postfix lmao
-        stack<long long>st;
+        //postFix conversion !? Hm.
+        stack<int>st;
         int n=tokens.size();
+        
         for(int i=0;i<n;++i){
-            if(tokens[i]=="+"){
+            if(isOperand(tokens[i])){
                 int x=st.top();
                 st.pop();
                 int y=st.top();
                 st.pop();
-                st.push(x+y);
-            }
-            else if(tokens[i]=="-"){
-                  int x=st.top();
-                st.pop();
-                int y=st.top();
-                st.pop();
-                //reverse order a-b since stack so b-a in addition and * we dont care order
-                st.push(y-x);
-            }
-            else if(tokens[i]=="*"){
-                long long x=st.top();
-                st.pop();
-                long long y=st.top();
-                st.pop();
-                long long temp=x*y;
-                st.push(temp);
-            }
-            else if(tokens[i]=="/"){
-                  int x=st.top();
-                st.pop();
-                int y=st.top();
-                st.pop();
-                st.push(y/x);
+                
+                //guranteed 2 elements will be there hm
+                string operand=tokens[i];
+                
+                if(operand=="+"){
+                    st.push(x+y);
+                }
+                else if(operand=="-"){
+                    st.push(y-x);
+                }
+                else if(operand=="/"){
+                    st.push(y/x);
+                }
+                else{
+                    st.push(x*y);
+                }
             }
             else{
-                
                 st.push(stoi(tokens[i]));
             }
         }
+        
         return st.top();
         
     }
