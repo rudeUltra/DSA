78265a440f2c864014e52c108126b5ef09c58427
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-01-31 14:32:46 +0530

    Time: 116 ms (93.33%), Space: 105.5 MB (25.62%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        //Kiliye kiliye
+        
+        //moNoTonic Steck
         int n=temperatures.size();
+        //This is basically next greater AAi
         vector<int>ans(n,0);
+        
         stack<int>st;
+        
         for(int i=0;i<n;++i){
             while(!st.empty() && temperatures[st.top()]<temperatures[i]){
-                ans[st.top()]=i-st.top();
+                //gotemm
+                ans[st.top()]=abs(st.top()-i);
                 st.pop();
             }
             st.push(i);
         }
+        
         return ans;
     }
 };
