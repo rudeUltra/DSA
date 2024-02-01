6b37ae5f86c12f48a6d541e7fca69758d786c052
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-02-01 21:36:05 +0530

    Time: 28 ms (63.31%), Space: 30.6 MB (13.22%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        //min type subarray siu
+        //If we have figured out O(n) try nlogn ? Wow.
+        
         int n=nums.size();
         int left=0;
         int right=0;
+        
         int sum=0;
+        
+        //mInIMAL bOiii
         int ans=INT_MAX;
-        while(right<n){
+        
+        while(right<n && left<=right){
             sum+=nums[right];
+            
+            //check Invalid Gotemm
             while(sum>=target){
-                ans=min(ans,(right-left+1));
-                sum=sum-nums[left];
-                left++; //keep minimizing 
-            }
+                ans=min(ans,(right-left)+1);
+                sum-=nums[left];
+                left++;
+                
+            } 
+            
             right++;
         }
+        
         return ans==INT_MAX?0:ans;
     }
 };
