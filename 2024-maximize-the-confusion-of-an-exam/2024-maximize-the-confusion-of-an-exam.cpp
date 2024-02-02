5734939f5fbf967056bee891b75bf08aaa5331e1
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-02-02 14:57:04 +0530

    Time: 112 ms (5.09%), Space: 12.6 MB (10.49%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -19,6 +19,8 @@ public:
             
             while((right-left+1)-mx>k){
                 
+                cout<<"Called";
+                
                 mp[answerKey[left]]--;
                 left++;
             }
