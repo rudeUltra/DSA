0a1baabbd20ea9d27b015e225b3aa0e5776ad33c
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-02-11 21:52:04 +0530

    Time: 319 ms (28.41%), Space: 88.4 MB (90.86%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -1,29 +1,34 @@
 /**
  * @param {Function} fn
+ * @return {Function}
  */
 function memoize(fn) {
+    const cache={}; //objeCT NOice
     
-   const cache = {};
-  
-   return function(...args) {
-    const key = JSON.stringify(args);
-    
-    if (key in cache) {
-      return cache[key];
+    return function(...args) {
+        let key=JSON.stringify(args);
+        
+        if(key in cache){
+            return cache[key]; //MeMoization boi
+        }
+        //Json is the string version of an Object NOice hm.
+        //.json converts JSON into an object hM.
+        
+        let temp=fn(...args);
+        cache[key]=temp;
+        
+        return cache[key];
     }
-    
-    const result = fn(...args);
-    cache[key] = result;
-    
-    return result;
-  }
-  
 }
 
 
-const memoizedSum = memoize(function(a, b) {
-  return a + b;
-});
-
-console.log(memoizedSum(2, 3)); // Output: Computing sum, 5
-console.log(memoizedSum(2, 3)); // Output: 5
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
