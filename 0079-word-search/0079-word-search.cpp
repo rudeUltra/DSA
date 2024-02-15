7ffbfa456772ca50a3ed5812167e39909c919eac
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-02-15 14:26:10 +0530

    Time: 264 ms (84.64%), Space: 9.7 MB (37.39%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,53 +2,52 @@ class Solution {
 public:
     
     
-    bool dfs(int i,int j,vector<vector<char>>&board,string &word,vector<vector<char>>&vis,int k){
-        if(k==word.size()){
-            return true; //gotemm
+    
+    bool dfs(int i,int j,vector<vector<char>>&board,string &word,int idx,vector<vector<int>>&visited){
+        
+         if(idx==word.size()){
+            return true; //all Matched
         }
         
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || vis[i][j]==1 || board[i][j]!=word[k]){
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || visited[i][j]==1 || board[i][j]!=word[idx]){
             return false;
         }
         
-        vis[i][j]=1;
-        bool ans=false;
-        ans=ans | dfs(i+1,j,board,word,vis,k+1);
-        ans=ans |dfs(i-1,j,board,word,vis,k+1);
-        ans=ans | dfs(i,j+1,board,word,vis,k+1);
-        ans=ans | dfs(i,j-1,board,word,vis,k+1);
+      
         
-        //backtrack
-        vis[i][j]=0;
+        visited[i][j]=1; //Hm.
         
-        return ans;
+        bool up=dfs(i-1,j,board,word,idx+1,visited);
+        bool down=dfs(i+1,j,board,word,idx+1,visited);
+        bool right=dfs(i,j+1,board,word,idx+1,visited);
+        bool left=dfs(i,j-1,board,word,idx+1,visited);
         
+        if(up || down || right || left){
+            return true;
+        }
+        
+        //sed so Backtrack
+        visited[i][j]=0;
+        
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        
-        
-        //DFS + Backtrack f
-        //Let's do the Harder DP list 
-        
-        //visit if no success then trackback siu
         int n=board.size();
         int m=board[0].size();
         
-     
+        
         
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(board[i][j]==word[0]){
-                    vector<vector<char>>vis(n+1,vector<char>(m+1,-1));
-                    if(dfs(i,j,board,word,vis,0)){
+                    //AHA possible starting point for DFS noice
+                    vector<vector<int>>visited(n,vector<int>(m,0));
+                    if(dfs(i,j,board,word,0,visited)){
                         return true;
                     }
                 }
             }
         }
-        
-        return false; //sEd \U0001f614
-        
-        
+        return false; //nahi mIla sad
     }
 };
