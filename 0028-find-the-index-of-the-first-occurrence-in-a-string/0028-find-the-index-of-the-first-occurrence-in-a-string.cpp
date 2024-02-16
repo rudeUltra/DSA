af18766d13b4139389af77103adf33f8e3f868d4
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-02-16 15:12:01 +0530

    Time: 4 ms (12.27%), Space: 7.8 MB (13.88%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,52 +1,72 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n=haystack.length();
-        int m=needle.length();
-        vector<int>lps(m,0);
-        int prevlps=0;
-        int i=1;
+    
+    
+    
+    vector<int> Zfunction(string &ans){
+        //O(N) time complexity noice
+        int n=ans.size();
+        vector<int>z(n,0);
+        z[0]=0; //sED
+        int left=0;
+        int right=0;
         
-        while(i<m){
-            if(needle[i]==needle[prevlps]){
-                lps[i]=1+prevlps;
-                prevlps++;
-                i++;
-            }
-            else if(prevlps==0){
-                lps[i]=0;
-                i++;
+        for(int k=1;k<n;++k){
+            if(k>right){
+                //sED
+                left=right=k;
+                
+                while(right<n && ans[right-left]==ans[right]){
+                    right++;
+                }
+                
+                z[k]=right-left;
+                right--;
             }
             else{
-                prevlps=lps[prevlps-1];
-            }
-        }
-        
-        i=0;
-        int j=0;
-        
-        while(i<n){
-            if(haystack[i]==needle[j]){
-                ++i;
-                ++j;
-            }
-            
-            else{
-                if(j==0){
-                    ++i;
+                //try to get the answer from the window Hm.
+                int k1=k-left;
+                if(z[k1]+k<=right){
+                    //Reuse the values hm.
+                    z[k]=z[k1];
                 }
                 else{
-                    j=lps[j-1];
+                    left=k;
+                    while(right<n && ans[right-left]==ans[right]){
+                        right++;
+                    }
+                    z[k]=right-left;
+                    right--;
                 }
-            }
-            
-            
-            if(j==m){
-                return i-m;
             }
         }
         
-        return -1;
+        return z;
         
+        
+    }
+    
+    
+    
+    int strStr(string haystack, string needle) {
+        //let's solve it using Z-function noice
+        
+        //Z-function is based on creating a Z array so that longest substring at index k which is also a prefix of the entire array Hm noice
+        
+        string ans=needle+'$'+haystack;
+        
+        vector<int>z=Zfunction(ans);
+        
+        int n=haystack.size();
+        int m=needle.size();
+        for(int i=0;i<(n+m+1);++i){
+            cout<<z[i]<<" ";
+            if(z[i]==m){
+                return i-(m+1); //gOteMM
+            }
+        }
+        
+        
+        return -1;
     }
 };
