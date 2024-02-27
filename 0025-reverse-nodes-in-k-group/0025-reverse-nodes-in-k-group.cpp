1a2f731a3391e0f8fc2b9b09c7f88daed62852a4
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-02-27 15:38:48 +0530

    Time: 7 ms (88.83%), Space: 15.1 MB (24.98%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,42 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *dummy=new ListNode(0,head);
-        ListNode *gprev=dummy; //group previous
-        
-        while(1){
-            ListNode *kth=getkth(gprev,k);
-            if(kth==NULL){
-                break; //k size group doesnt exist so break loop
-            }
-            //k size group exists so perform reverse
-            ListNode *gnext=kth->next;
-            
-            //reverse
-            
-            ListNode *prev=kth->next;
-            ListNode *curr=gprev->next;
-            while(curr!=gnext){
-                ListNode *temp=curr->next;
-                curr->next=prev;
-                prev=curr;
-                curr=temp;
-            }
-            
-            //after reversing
-            ListNode *temp=gprev->next; //K size group reversed move to the next
-            gprev->next=kth;
-            gprev=temp;
-            
-        }
-        return dummy->next;
-    }
-    ListNode* getkth(ListNode *curr,int k){
-        while(curr && k>0){
+    ListNode* getKth(ListNode*gprev,int k){
+        ListNode*curr=gprev;
+        while(k && curr!=NULL){
             curr=curr->next;
             k--;
         }
         return curr;
+    }
+    
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode*ans=new ListNode(0);
+        ListNode*gprev=ans;
+        gprev->next=head; //connected nOice
+        
+        while(1){
+            ListNode*kth=getKth(gprev,k);
+            
+            if(kth==NULL){
+                //Not enough NOdes sed
+                break;
+            }
+            
+            ListNode*gnext=kth->next;
+            
+            //reverse Now NOice
+            ListNode*prev=gnext; //nOICE not null like normal Reverse AahA.
+            ListNode*curr=gprev->next;
+            while(curr && curr!=gnext){
+                ListNode*temp=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=temp; 
+            }
+            
+            //reverseD nOice now what.
+            ListNode*temp=gprev->next;
+            gprev->next=kth;
+            gprev=temp;
+            
+        }
+        
+        return ans->next;
     }
 };
