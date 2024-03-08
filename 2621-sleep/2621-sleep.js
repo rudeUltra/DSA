d12bb91d43cad157aa6531665c10283aabddc3bd
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-09 00:13:25 +0530

    Time: 53 ms (53.88%), Space: 48 MB (75.82%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return await new Promise((resolve,reject)=>{
+        setTimeout(resolve,millis);
+    })
+    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
