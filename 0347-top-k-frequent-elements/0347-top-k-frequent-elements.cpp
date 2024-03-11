c487afe5537ee6c19ce3bcdf00146acfa47db8e7
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-11 21:54:57 +0530

    Time: 12 ms (63.05%), Space: 17.2 MB (31.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        //hash + heap
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         int n=nums.size();
+        vector<int>ans;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; //min heap to maintain K size hm
         unordered_map<int,int>mp;
+        
         for(int i=0;i<n;++i){
             mp[nums[i]]++;
         }
         
         for(auto it:mp){
-            if(pq.size()==0 || pq.size()<k){
+            if(pq.size()<k){
                 pq.push({it.second,it.first});
             }
             else{
+                //Hm.
                 if(pq.top().first<it.second){
+                    //Or i can just check with a normal if else ok anyway.
                     pq.pop();
                     pq.push({it.second,it.first});
                 }
             }
-            
         }
-        vector<int>ans;
-        while(!pq.empty() && k){
-            ans.push_back(pq.top().second);
+        
+        //AAHa noice
+        while(!pq.empty()){
+            auto it=pq.top();
             pq.pop();
+            ans.push_back(it.second);
         }
         
-        //nlogK
         return ans;
-        
-        
-        
     }
 };
