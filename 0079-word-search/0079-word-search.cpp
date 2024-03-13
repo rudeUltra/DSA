84619c5a8649f9e172b1131f48c3af3b07b905e1
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-13 11:24:48 +0530

    Time: 1080 ms (14.15%), Space: 12 MB (7.36%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,30 +3,29 @@ public:
     
     
     
-    bool dfs(int i,int j,vector<vector<char>>&board,string &word,int idx,vector<vector<int>>&visited){
-        
-         if(idx==word.size()){
-            return true; //all Matched
-        }
-        
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || visited[i][j]==1 || board[i][j]!=word[idx]){
-            return false;
-        }
-        
-      
-        
-        visited[i][j]=1; //Hm.
-        
-        bool up=dfs(i-1,j,board,word,idx+1,visited);
-        bool down=dfs(i+1,j,board,word,idx+1,visited);
-        bool right=dfs(i,j+1,board,word,idx+1,visited);
-        bool left=dfs(i,j-1,board,word,idx+1,visited);
-        
-        if(up || down || right || left){
+    
+    bool dfs(int idx,int i,int j,vector<vector<char>>&board,string word,vector<vector<int>>&visited){
+        if(idx==word.size()){
+            //less Goo
             return true;
         }
         
-        //sed so Backtrack
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || word[idx]!=board[i][j] || visited[i][j]==1){
+            return false; //Moy6e Moye
+        }
+        
+        visited[i][j]=1; //ahA
+        
+        bool down=dfs(idx+1,i+1,j,board,word,visited);
+        bool right=dfs(idx+1,i,j+1,board,word,visited);
+        bool left=dfs(idx+1,i,j-1,board,word,visited);
+        bool up=dfs(idx+1,i-1,j,board,word,visited);
+        
+        if(down || right || left || up){
+            return true;
+        }
+        
+        //Maa chuda...
         visited[i][j]=0;
         
         return false;
@@ -35,19 +34,22 @@ public:
         int n=board.size();
         int m=board[0].size();
         
-        
+        //Before each call make a visited nOice
         
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(board[i][j]==word[0]){
-                    //AHA possible starting point for DFS noice
-                    vector<vector<int>>visited(n,vector<int>(m,0));
-                    if(dfs(i,j,board,word,0,visited)){
+                    //nOICE
+                    vector<vector<int>>visited(n,vector<int>(m+1,0));
+                    
+                    if(dfs(0,i,j,board,word,visited)){
                         return true;
                     }
                 }
             }
         }
-        return false; //nahi mIla sad
+        
+        return false; //nahi MilA F.
+        
     }
 };
