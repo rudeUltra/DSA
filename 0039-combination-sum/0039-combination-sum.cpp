db7c7ed5c08cff7856591960218c5670e4c532fe
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-13 11:15:44 +0530

    Time: 7 ms (50.56%), Space: 16 MB (39.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     
-    void f(int idx,vector<int>&candidates,int target,vector<int>&temp,vector<vector<int>>&ans){
+    
+    
+    //Basically glorified pick not pick NoICe
+    
+    void f(vector<int>&candidates,int idx,int target,vector<vector<int>>&ans,vector<int>&temp){
         if(target==0){
+            //gotemm
             ans.push_back(temp);
             return;
         }
-        if(idx==candidates.size()){
-            return;
+        
+        if(idx>=candidates.size()){
+            return; //Moye Moye.
         }
-        //Notpick
-        f(idx+1,candidates,target,temp,ans);
+        
+        //not pick
+        f(candidates,idx+1,target,ans,temp);
+        
         //Pick
         if(candidates[idx]<=target){
             temp.push_back(candidates[idx]);
-            f(idx,candidates,target-candidates[idx],temp,ans);
-            temp.pop_back();
+            f(candidates,idx,target-candidates[idx],ans,temp);
+            temp.pop_back(); //backTrack Noice
         }
-        
     }
+    
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>temp;
         vector<vector<int>>ans;
+        vector<int>temp;
+        int n=candidates.size();
         
-        f(0,candidates,target,temp,ans);
+        f(candidates,0,target,ans,temp);
         
         return ans;
     }
