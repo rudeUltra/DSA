edc16e0b92f6ae44ab72a668d4338bab4aba6d6d
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-14 15:40:03 +0530

    Time: 94 ms (14.61%), Space: 38.4 MB (20.69%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        //same concept bc siuuU prefix sum
+        //No of subarrays SO! prefix sum would be better i guess
+        int n=nums.size();
         unordered_map<int,int>mp;
         int sum=0;
-        int n=nums.size();
         int count=0;
-
         for(int i=0;i<n;++i){
-            sum=sum+nums[i];
-
+            sum+=nums[i];
+            cout<<sum<<" ";
+            
             if(sum==goal){
+                //gOTemm
                 count++;
             }
+            
             if(mp.find(sum-goal)!=mp.end()){
+                //exists hm.
                 count+=mp[sum-goal];
             }
             mp[sum]++;
         }
+        
         return count;
-
     }
 };
