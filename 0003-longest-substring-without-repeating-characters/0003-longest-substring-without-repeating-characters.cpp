df8a0b909a6fb68158d299408ebc1c4b75a654a0
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-15 14:46:58 +0530

    Time: 7 ms (83.32%), Space: 11.1 MB (62.39%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //we can do if else here bcuz we can check the condition of valid directly SIU
         int n=s.size();
+        
+        unordered_map<char,int>mp;
+        
+        int ans=0;
+        
         int left=0;
         int right=0;
-        int ans=0;
-        unordered_map<int,int>mp;
         while(right<n){
-            if(mp[s[right]]==0){
-                mp[s[right]]++;
-                ans=max(ans,(right-left)+1);
-                right++;
-            }
-            else{
+            mp[s[right]]++;
+            //No RepeAt Hm.
+            while(mp[s[right]]>1){
                 mp[s[left]]--;
                 left++;
             }
+            
+            ans=max(ans,(right-left+1));
+            right++;
         }
-        
-        return ans;
-        
+        return ans; //max type hm.
     }
 };
