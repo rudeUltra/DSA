de37a7ccd71f81956e8fa0bf3b9786244c6c08cc
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-15 21:43:29 +0530

    Time: 0 ms (100.00%), Space: 15 MB (61.69%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    
-    void f(int open,int close,int n,vector<string>&ans,string temp){
-        if(open==n && close==n){
-            //done 
-            ans.push_back(temp);
+    void f(int idx,int open,int close,string s,int n,vector<string>&ans){
+        if(idx==2*n){
+            ans.push_back(s);
             return;
         }
         
         if(open<n){
-            f(open+1,close,n,ans,temp+'(');
+            //i can open NOice
+            f(idx+1,open+1,close,s+'(',n,ans);
         }
-        if(close<open){
-            f(open,close+1,n,ans,temp+')');
-        }
-    }
-    vector<string> generateParenthesis(int n) {
-        //Decision making 
-        //Drop rain hm
-        vector<string>ans;
-        f(0,0,n,ans,"");
         
+        if(close<open){
+            //wE have to close also bC nvm.
+            f(idx+1,open,close+1,s+')',n,ans);
+        }
+        
+        
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        //2*n is the base case hm.
+        vector<string>ans;
+        f(0,0,0,"",n,ans);
         return ans;
     }
 };
