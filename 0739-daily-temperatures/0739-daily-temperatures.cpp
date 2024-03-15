5250404336985c88535722d3a21642e012aaf2cb
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-15 21:48:53 +0530

    Time: 129 ms (31.81%), Space: 105.3 MB (52.13%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        //Kiliye kiliye
-        
-        //moNoTonic Steck
+        //jUst show up everyday that's what matters Hm.
         int n=temperatures.size();
-        //This is basically next greater AAi
-        vector<int>ans(n,0);
-        
         stack<int>st;
+        vector<int>nextGreater(n,0); //hM.
         
         for(int i=0;i<n;++i){
             while(!st.empty() && temperatures[st.top()]<temperatures[i]){
-                //gotemm
-                ans[st.top()]=abs(st.top()-i);
+                nextGreater[st.top()]=i-st.top(); //Diff in indexes Hm.
                 st.pop();
             }
             st.push(i);
         }
         
-        return ans;
+        return nextGreater;
     }
 };
