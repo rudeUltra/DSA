5fdc386afe7833669f3971acca34862947cf2f51
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-15 14:40:21 +0530

    Time: 9 ms (57.51%), Space: 9.3 MB (52.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n=s.size();
-        vector<int>hash(26,0);
+        int ans=0;
         
         int left=0;
         int right=0;
-        
-        int ans=0;
-        
+        unordered_map<char,int>mp;
         while(right<n){
-            hash[s[right]-'A']++;
+            mp[s[right]]++; //nOice
             
-            //Check invalid case hm.
-            int mx=0;
-            for(int i=0;i<26;++i){
-                mx=max(mx,hash[i]);
+            int mx=0; //Get the max freq HM.
+            //only 26 letters so gg Ez
+            for(auto it:mp){
+                mx=max(mx,it.second);
             }
             
-            if((right-left+1)-mx>k){
-                hash[s[left]-'A']--;
+            //Invalid case hm.
+            while((right-left+1)-mx>k){
+                //noPe
+                mp[s[left]]--;
                 left++;
             }
+            
+            //valid length Hm.
             ans=max(ans,(right-left+1));
             right++;
         }
+        
         return ans;
     }
 };
