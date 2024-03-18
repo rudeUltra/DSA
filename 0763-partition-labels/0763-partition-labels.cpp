da015df91d6a59646dcff1abaf489f3d204dc030
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-18 18:45:00 +0530

    Time: 4 ms (53.43%), Space: 9.3 MB (10.82%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int n = s.size();
-        unordered_map<char,int>mpp;
-        for(int i = 0; i < n; i++){
-            mpp[s[i]] = i;
+        int n=s.size();
+        unordered_map<char,int>mp;
+        unordered_map<char,int>hash;
+        
+        //Clear the map hm.
+        for(int i=0;i<n;++i){
+            mp[s[i]]=i;
         }
-        int maxi = 0, prev = -1;
         vector<int>ans;
-        for(int i = 0; i < n; i++){
-            maxi = max(maxi,mpp[s[i]]);     // updating maxi if current element's last occurence is greater than maxi
-            if(maxi == i){                          // since maxi == i it means last occurence of all elements till now is present in this partition so we add this partition to the vector.
-                ans.push_back(maxi - prev);
-                maxi = -1;
-                prev = i;
+        int unique=0;
+        int total=0;
+        int prev=-1;
+        for(int i=0;i<n;++i){
+            hash[s[i]]++;
+            if(hash[s[i]]==1){
+                unique++;
+            }
+            if(i==mp[s[i]]){
+                //last position hm
+                total++;
+            }
+            
+            if(unique==total){
+                //nOice
+                ans.push_back((i-prev));
+                unique=0;
+                total=0;
+                prev=i;
+                hash.clear();
             }
         }
         return ans;
