16d33b4ae8848b21dc3c039671030b9a498e3482
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-03-22 15:49:02 +0530

    Time: 50 ms (59.32%), Space: 48.2 MB (93.34%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,8 +1,12 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
 Array.prototype.last = function() {
     let n=this.length
     if(n==0){
         return -1;
     }
+    
     return this[n-1];
 };
 
