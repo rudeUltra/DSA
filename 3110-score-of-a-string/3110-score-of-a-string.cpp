3662baf78e197ec083c2fffad5069c042e6f805c
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2024-06-01 11:35:58 +0530

    Time: 5 ms (27.27%), Space: 7.8 MB (42.17%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n=s.size();
+        int ans=0;
+        
+        for(int i=0;i<n-1;++i){
+            ans+=abs(s[i+1]-s[i]);
+        }
+        
+        return ans;
+    }
+};
